4768d731547b6204872999a9cbbfe5410ab515ea
Shuping Cui <scui@redhat.com>
2014-03-17 12:30:49 +0800

    generic.tests.kdump: Add new test case for kdump over ssh
    Dumping the vmcore for remote system via ssh.
    
    Signed-off-by: Shuping Cui <scui@redhat.com>

--- generic/tests/kdump.py
+++ generic/tests/kdump.py
@@ -1,12 +1,55 @@
 import logging
+from autotest.client import utils
 from autotest.client.shared import error
-from virttest import utils_misc
+from virttest import utils_misc, utils_net, utils_conn
+
+
+def preprocess_kdump(vm, timeout):
+    """
+    Backup /etc/kdump.conf file before trigger crash.
+
+    :param timeout: Timeout in seconds
+    """
+    kdump_cfg_path = vm.params.get("kdump_cfg_path", "/etc/kdump.conf")
+    cp_kdumpcf_cmd = "/bin/cp -f %s %s-bk" % (kdump_cfg_path, kdump_cfg_path)
+    cp_kdumpcf_cmd = vm.params.get("cp_kdumpcf_cmd", cp_kdumpcf_cmd)
+
+    session = vm.wait_for_login(timeout=timeout)
+
+    logging.info("Backup kdump.conf file.")
+    status, output = session.cmd_status_output(cp_kdumpcf_cmd)
+    if status != 0:
+        logging.error(output)
+        raise error.TestError("Fail to backup the kdump.conf")
+
+    session.close()
+
+
+def postprocess_kdump(vm, timeout):
+    """
+    Restore /etc/kdump.conf file after trigger crash.
+
+    :param timeout: Timeout in seconds
+    """
+    kdump_cfg_path = vm.params.get("kdump_cfg_path", "/etc/kdump.conf")
+    restore_kdumpcf_cmd = "/bin/cp -f %s-bk %s" % (kdump_cfg_path, kdump_cfg_path)
+    restore_kdumpcf_cmd = vm.params.get("restore_kdumpcf_cmd", restore_kdumpcf_cmd)
+
+    session = vm.wait_for_login(timeout=timeout)
+
+    logging.info("Restore kdump.conf")
+    status, output = session.cmd_status_output(restore_kdumpcf_cmd)
+    if status != 0:
+        logging.error(output)
+        raise error.TestError("Fail to restore the kdump.conf")
+
+    session.close()
 
 
 def kdump_enable(vm, vm_name, crash_kernel_prob_cmd,
                  kernel_param_cmd, kdump_enable_cmd, timeout):
     """
-    Check, configure and enable the kdump
+    Check, configure and enable the kdump in guest.
 
     :param vm_name: vm name
     :param crash_kernel_prob_cmd: check kdume loaded
@@ -14,6 +57,12 @@ def kdump_enable(vm, vm_name, crash_kernel_prob_cmd,
     :param kdump_enable_cmd: enable kdump command
     :param timeout: Timeout in seconds
     """
+    kdump_cfg_path = vm.params.get("kdump_cfg_path", "/etc/kdump.conf")
+    kdump_config = vm.params.get("kdump_config")
+    vmcore_path = vm.params.get("vmcore_path", "/var/crash")
+    kdump_method = vm.params.get("kdump_method", "basic")
+    kdump_propagate_cmd = vm.params.get("kdump_propagate_cmd")
+
     error.context("Try to log into guest '%s'." % vm_name, logging.info)
     session = vm.wait_for_login(timeout=timeout)
 
@@ -27,13 +76,40 @@ def kdump_enable(vm, vm_name, crash_kernel_prob_cmd,
         session.cmd(kernel_param_cmd)
         session = vm.reboot(session, timeout=timeout)
 
-    if vm.params.get("kdump_config"):
-        error.context("Configuring the Core Collector", logging.info)
-        config_file = "/etc/kdump.conf"
-        for config_line in vm.params.get("kdump_config").split(";"):
-            config_cmd = "grep '^%s$' %s || echo -e '%s' >> %s "
+    if kdump_config:
+        if kdump_method == "ssh":
+            host_ip = utils_net.get_ip_address_by_interface(vm.params.get('netdst'))
+            kdump_config = kdump_config % (host_ip, vmcore_path)
+
+        error.context("Configuring the Core Collector...", logging.info)
+
+        session.cmd("cat /dev/null > %s" % kdump_cfg_path)
+        for config_line in kdump_config.split(";"):
+            config_cmd = "echo -e '%s' >> %s "
             config_con = config_line.strip()
-            session.cmd(config_cmd % ((config_con, config_file) * 2))
+            session.cmd(config_cmd % (config_con, kdump_cfg_path))
+
+    if kdump_method == "ssh":
+        host_pwd = vm.params.get("host_pwd", "redhat")
+        guest_pwd = vm.params.get("guest_pwd", "redhat")
+        guest_ip = vm.get_address()
+
+        error.context("Setup ssh login without password...", logging.info)
+        session.cmd("rm -rf /root/.ssh/*")
+
+        ssh_connection = utils_conn.SSHConnection(server_ip=host_ip,
+                                                  server_pwd=host_pwd,
+                                                  client_ip=guest_ip,
+                                                  client_pwd=guest_pwd)
+        try:
+            ssh_connection.conn_check()
+        except utils_conn.ConnectionError:
+            ssh_connection.conn_setup()
+            ssh_connection.conn_check()
+
+        logging.info("Trying to propagate with command '%s'" %
+                     kdump_propagate_cmd)
+        session.cmd(kdump_propagate_cmd, timeout=120)
 
     error.context("Enabling kdump service...", logging.info)
     # the initrd may be rebuilt here so we need to wait a little more
@@ -50,18 +126,33 @@ def crash_test(vm, vcpu, crash_cmd, timeout):
     :param crash_cmd: crash_cmd which is triggered crash command
     :param timeout: Timeout in seconds
     """
+    vmcore_path = vm.params.get("vmcore_path", "/var/crash")
+    kdump_method = vm.params.get("kdump_method", "basic")
+    vmcore_rm_cmd = vm.params.get("vmcore_rm_cmd", "rm -rf %s/*")
+    vmcore_rm_cmd = vmcore_rm_cmd % vmcore_path
+    kdump_restart_cmd = vm.params.get("kdump_restart_cmd",
+                                      "service kdump restart")
+
     session = vm.wait_for_login(timeout=timeout)
 
     logging.info("Delete the vmcore file.")
-    session.cmd_output("rm -rf /var/crash/*")
-
-    if crash_cmd == "nmi":
-        logging.info("Triggering crash with 'nmi' interrupt")
-        session.cmd("echo 1 > /proc/sys/kernel/unknown_nmi_panic")
-        vm.monitor.nmi()
+    if kdump_method == "ssh":
+        utils.run(vmcore_rm_cmd)
     else:
-        logging.info("Triggering crash on vcpu %d ...", vcpu)
-        session.sendline("taskset -c %d %s" % (vcpu, crash_cmd))
+        session.cmd_output(vmcore_rm_cmd)
+
+    session.cmd(kdump_restart_cmd, timeout=120)
+
+    try:
+        if crash_cmd == "nmi":
+            logging.info("Triggering crash with 'nmi' interrupt")
+            session.cmd("echo 1 > /proc/sys/kernel/unknown_nmi_panic")
+            vm.monitor.nmi()
+        else:
+            logging.info("Triggering crash on vcpu %d ...", vcpu)
+            session.sendline("taskset -c %d %s" % (vcpu, crash_cmd))
+    except Exception:
+        postprocess_kdump(vm, timeout)
 
 
 def check_vmcore(vm, session, timeout):
@@ -71,18 +162,26 @@ def check_vmcore(vm, session, timeout):
     :param session: A shell session object or None.
     :param timeout: Timeout in seconds
     """
-    if not utils_misc.wait_for(lambda: not session.is_responsive(), 240, 0,
-                               1):
-        raise error.TestFail("Could not trigger crash")
+    vmcore_path = vm.params.get("vmcore_path", "/var/crash")
+    vmcore_chk_cmd = vm.params.get("vmcore_chk_cmd", "ls -R %s | grep vmcore")
+    vmcore_chk_cmd = vmcore_chk_cmd % vmcore_path
 
-    error.context("Waiting for kernel crash dump to complete",
-                  logging.info)
+    if not utils_misc.wait_for(lambda: not session.is_responsive(), 240, 0, 1):
+        raise error.TestFail("Could not trigger crash.")
+
+    error.context("Waiting for kernel crash dump to complete", logging.info)
     session = vm.wait_for_login(timeout=timeout)
 
     error.context("Probing vmcore file...", logging.info)
     try:
-        session.cmd("ls -R /var/crash | grep vmcore")
+        if vm.params.get("kdump_method") == "ssh":
+            logging.info("Checking vmcore file on host")
+            utils.run(vmcore_chk_cmd)
+        else:
+            logging.info("Checking vmcore file on guest")
+            session.cmd(vmcore_chk_cmd)
     except Exception:
+        postprocess_kdump(vm, timeout)
         raise error.TestFail("Could not found vmcore file.")
 
     logging.info("Found vmcore.")
@@ -117,28 +216,40 @@ def run(test, params, env):
     vm_list = []
     session_list = []
 
-    for vm_name in vms:
-        vm = env.get_vm(vm_name)
-        vm.verify_alive()
-        vm_list.append(vm)
+    try:
+        for vm_name in vms:
+            vm = env.get_vm(vm_name)
+            vm.verify_alive()
+            vm_list.append(vm)
 
-        session = kdump_enable(vm, vm_name, crash_kernel_prob_cmd,
-                               kernel_param_cmd, kdump_enable_cmd, timeout)
-        session_list.append(session)
+            preprocess_kdump(vm, timeout)
 
-    for vm in vm_list:
-        error.context("Kdump Testing, force the Linux kernel to crash",
-                      logging.info)
-        crash_cmd = params.get("crash_cmd", "echo c > /proc/sysrq-trigger")
-        if crash_cmd == "nmi":
-            crash_test(vm, None, crash_cmd, timeout)
-        else:
-            # trigger crash for each vcpu
-            nvcpu = int(params.get("smp", 1))
-            for i in range(nvcpu):
-                crash_test(vm, i, crash_cmd, timeout)
+            session = kdump_enable(vm, vm_name, crash_kernel_prob_cmd,
+                                   kernel_param_cmd, kdump_enable_cmd, timeout)
 
-    for i in range(len(vm_list)):
-        error.context("Check the vmcore file after triggering a crash",
-                      logging.info)
-        check_vmcore(vm_list[i], session_list[i], crash_timeout)
+            session_list.append(session)
+
+        for vm in vm_list:
+            error.context("Kdump Testing, force the Linux kernel to crash",
+                          logging.info)
+            crash_cmd = params.get("crash_cmd", "echo c > /proc/sysrq-trigger")
+
+            session = vm.wait_for_login(timeout=timeout)
+            if crash_cmd == "nmi":
+                crash_test(vm, None, crash_cmd, timeout)
+            else:
+                # trigger crash for each vcpu
+                nvcpu = int(params.get("smp", 1))
+                for i in range(nvcpu):
+                    crash_test(vm, i, crash_cmd, timeout)
+
+        for i in range(len(vm_list)):
+            error.context("Check the vmcore file after triggering a crash",
+                          logging.info)
+            check_vmcore(vm_list[i], session_list[i], crash_timeout)
+    finally:
+        for s in session_list:
+            s.close()
+        for vm in vm_list:
+            postprocess_kdump(vm, timeout)
+            vm.destroy()

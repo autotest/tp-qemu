9526656591da76421e8bb53a5fb2778a93f8e851
Jonas Eriksson <jonas.eriksson@enea.com>
2014-04-14 17:31:41 +0200

    clock_getres: Migrate to remote_build
    Tested by running: clock_getres
    
    Signed-off-by: Jonas Eriksson <jonas.eriksson@enea.com>

--- generic/tests/clock_getres.py
+++ generic/tests/clock_getres.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from autotest.client.shared import error
-from virttest import utils_test, data_dir
+from virttest import utils_test, data_dir, remote_build
 
 
 @error.context_aware
@@ -20,21 +20,17 @@ def run(test, params, env):
     timeout = int(params.get("login_timeout", 360))
     session = vm.wait_for_login(timeout=timeout)
 
-    getres_cmd = params.get("getres_cmd")
+    address = vm.get_address(0)
+    source_dir = data_dir.get_deps_dir("clock_getres")
+    build_dir = params.get("build_dir", None)
 
-    if not getres_cmd or session.cmd_status("test -x %s" % getres_cmd):
-        source_name = "clock_getres/clock_getres.c"
-        source_name = os.path.join(data_dir.get_deps_dir(), source_name)
-        getres_cmd = "/tmp/clock_getres"
-        dest_name = "/tmp/clock_getres.c"
+    builder = remote_build.Builder(params, address, source_dir,
+                                   build_dir=build_dir)
 
-        if not os.path.isfile(source_name):
-            raise error.TestError("Could not find %s" % source_name)
+    getres_cmd = os.path.join(builder.build(), "clock_getres")
 
-        vm.copy_files_to(source_name, dest_name)
-        session.cmd("gcc -lrt -o %s %s" % (getres_cmd, dest_name))
-
-    session.cmd(getres_cmd)
+    if not session.cmd_status(getres_cmd) == 0:
+        raise Exception("clock_getres failed")
     logging.info("PASS: Guest reported appropriate clock resolution")
     sub_test = params.get("sub_test")
     if sub_test:

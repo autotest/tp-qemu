fd1db3ba91c2725f9d4514e23da60ee8ec71e7de
Feng Yang <fyang@redhat.com>
2014-05-04 11:28:49 +0800

    qemu.tests.netperf_stress: Support multi vms and multi servers/clients.
    Signed-off-by: Feng Yang <fyang@redhat.com>

--- qemu/tests/netperf_stress.py
+++ qemu/tests/netperf_stress.py
@@ -9,119 +9,202 @@ from virttest import utils_net, utils_netperf, utils_misc, data_dir
 @error.context_aware
 def run(test, params, env):
     """
-    Run netperf on server and client side, we need run this case on two
-    machines. if dsthost is not set will start netperf server on local
-    host.
-    Netperf stress test will keep running, until stress timeout or
-    env["netperf_run"] is False
+    Run netperf stress on server and client side.
 
-    1) Start one vm guest os as server.
-    2) Start a reference machine (dsthost) as client.
+    1) Start multi vm(s) guest.
+    2) Select multi vm(s) or host to setup netperf server/client.
     3) Run netperf stress test.
-    4) Finish test until timeout or env["netperf_run"] is False.
+    4) Finish test until timeout env["netperf_run"] is False.
 
     :param test: QEMU test object
     :param params: Dictionary with the test parameters
     :param env: Dictionary with test environment.
     """
     login_timeout = float(params.get("login_timeout", 360))
-    dsthost = params.get("dsthost", "localhost")
 
-    vm = env.get_vm(params["main_vm"])
-    vm.verify_alive()
-    session = vm.wait_for_login(timeout=int(params.get("login_timeout", 360)))
-    session.cmd("service iptables stop; iptables -F", ignore_all_errors=True)
+    netperf_server = params.get("netperf_server").split()
+    netperf_client = params.get("netperf_client").split()
+    guest_username = params.get("username", "")
+    guest_password = params.get("password", "")
+    host_password = params.get("hostpassword", "redhat")
+    shell_client = params.get("shell_client")
+    shell_port = params.get("shell_port")
+    os_type = params.get("os_type")
+    host_ip = utils_net.get_host_ip_address(params)
 
-    if dsthost in params.get("vms", "vm1 vm2"):
-        server_vm = env.get_vm(dsthost)
-        server_vm.verify_alive()
-        server_vm.wait_for_login(timeout=login_timeout)
-        dsthost_ip = server_vm.get_address()
-    elif re.match(r"((\d){1,3}\.){3}(\d){1,3}", dsthost):
-        dsthost_ip = dsthost
-    else:
-        server_interface = params.get("netdst", "switch")
-        host_nic = utils_net.Interface(server_interface)
-        dsthost_ip = host_nic.get_ip()
+    vms = params.get("vms")
+    server_infos = []
+    client_infos = []
+    for server in netperf_server:
+        s_info = {}
+        if server in vms:
+            server_vm = env.get_vm(server)
+            server_vm.verify_alive()
+            session = server_vm.wait_for_login(timeout=login_timeout)
+            session.cmd("service iptables stop; iptables -F",
+                        ignore_all_errors=True)
+            server_ip = server_vm.get_address()
+            s_info["ip"] = server_ip
+            s_info["os_type"] = params.get("os_type_%s" % server, os_type)
+            s_info["username"] = params.get("username_%s" % server,
+                                            guest_username)
+            s_info["password"] = params.get("password_%s" % server,
+                                            guest_password)
+            s_info["shell_client"] = params.get("shell_client_%s" % server,
+                                                shell_client)
+            s_info["shell_port"] = params.get("shell_port_%s" % server,
+                                              shell_port)
+        else:
+            if server == "localhost":
+                s_info["ip"] = host_ip
+                s_info["password"] = params.get("password_%s" % server,
+                                                host_password)
+            else:
+                s_info["ip"] = server
+                s_info["password"] = params.get("password_%s" % server,
+                                                "redhat")
+            s_info["os_type"] = params.get("os_type_%s" % server, "linux")
+            s_info["username"] = params.get("username_%s" % server,
+                                            "root")
+            s_info["shell_client"] = params.get("shell_client_%s" % server,
+                                                "ssh")
+            s_info["shell_port"] = params.get("shell_port_%s" % server,
+                                              "22")
+        server_infos.append(s_info)
+
+    for client in netperf_client:
+        c_info = {}
+        if client in vms:
+            client_vm = env.get_vm(client)
+            client_vm.verify_alive()
+            session = client_vm.wait_for_login(timeout=login_timeout)
+            session.cmd("service iptables stop; iptables -F",
+                        ignore_all_errors=True)
+            client_ip = client_vm.get_address()
+            c_info["ip"] = client_ip
+            c_info["os_type"] = params.get("os_type_%s" % server, os_type)
+            c_info["username"] = params.get("username_%s" % server,
+                                            guest_username)
+            c_info["password"] = params.get("password_%s" % server,
+                                            guest_password)
+            c_info["shell_client"] = params.get("shell_client_%s" % server,
+                                                shell_client)
+            c_info["shell_port"] = params.get("shell_port_%s" % server,
+                                              shell_port)
+        else:
+            if client == "localhost":
+                c_info["ip"] = host_ip
+                c_info["password"] = params.get("password_%s" % server,
+                                                host_password)
+            else:
+                c_info["ip"] = client
+                c_info["password"] = params.get("password_%s" % server,
+                                                "redhat")
+            c_info["os_type"] = params.get("os_type_%s" % server, "linux")
+            c_info["username"] = params.get("username_%s" % server,
+                                            "root")
+            c_info["shell_client"] = params.get("shell_client_%s" % server,
+                                                "ssh")
+            c_info["shell_port"] = params.get("shell_port_%s" % server,
+                                              "23")
+        client_infos.append(c_info)
 
     netperf_link = params.get("netperf_link")
     netperf_link = os.path.join(data_dir.get_deps_dir("netperf"), netperf_link)
     md5sum = params.get("pkg_md5sum")
-    netperf_server_link = params.get("netperf_server_link", netperf_link)
+    netperf_server_link = params.get("netperf_server_link_win", netperf_link)
     netperf_server_link = os.path.join(data_dir.get_deps_dir("netperf"),
                                        netperf_server_link)
-    server_md5sum = params.get("server_md5sum", md5sum)
-    server_path = params.get("server_path", "/var/tmp")
-    client_path = params.get("client_path", "/var/tmp")
+    server_md5sum = params.get("server_md5sum")
+    netperf_client_link = params.get("netperf_client_link_win", netperf_link)
+    netperf_client_link = os.path.join(data_dir.get_deps_dir("netperf"),
+                                       netperf_client_link)
+    client_md5sum = params.get("client_md5sum")
 
-    guest_usrname = params.get("username", "")
-    guest_passwd = params.get("password", "")
-    host_passwd = params.get("hostpasswd")
-    client = params.get("shell_client")
-    port = params.get("shell_port")
+    server_path_linux = params.get("server_path", "/var/tmp")
+    client_path_linux = params.get("client_path", "/var/tmp")
+    server_path_win = params.get("server_path_win", "c:\\")
+    client_path_win = params.get("client_path_win", "c:\\")
 
-    # main vm run as server when vm_as_server is 'yes'.
-    if params.get("vm_as_server", "yes") == "yes":
-        netserver_ip = vm.get_address()
-        netperf_client_ip = dsthost_ip
-    else:
-        netserver_ip = dsthost_ip
-        netperf_client_ip = vm.get_address()
+    netperf_clients = []
+    netperf_servers = []
+    for c_info in client_infos:
+        if c_info["os_type"] == "windows":
+            netperf_link_c = netperf_client_link
+            client_path = client_path_win
+        else:
+            netperf_link_c = netperf_link
+            client_path = client_path_linux
+        n_client = utils_netperf.NetperfClient(c_info["ip"],
+                                               client_path,
+                                               md5sum, netperf_link_c,
+                                               client=c_info["shell_client"],
+                                               port=c_info["shell_port"],
+                                               username=c_info["username"],
+                                               password=c_info["password"])
+        netperf_clients.append(n_client)
 
-    netperf_client = utils_netperf.NetperfClient(netperf_client_ip,
-                                                 client_path,
-                                                 md5sum, netperf_link,
-                                                 password=host_passwd)
-
-    netperf_server = utils_netperf.NetperfServer(netserver_ip,
-                                                 server_path,
-                                                 server_md5sum,
-                                                 netperf_server_link,
-                                                 client, port,
-                                                 username=guest_usrname,
-                                                 password=guest_passwd)
+    for s_info in server_infos:
+        if s_info["os_type"] == "windows":
+            netperf_link_s = netperf_server_link
+            server_path = server_path_win
+        else:
+            netperf_link_s = netperf_link
+            server_path = server_path_linux
+        n_server = utils_netperf.NetperfServer(s_info["ip"],
+                                               server_path,
+                                               md5sum, netperf_link_s,
+                                               client=s_info["shell_client"],
+                                               port=s_info["shell_port"],
+                                               username=s_info["username"],
+                                               password=s_info["password"])
+        netperf_servers.append(n_server)
 
     # Get range of message size.
     try:
-        netperf_server.start()
+        for n_server in netperf_servers:
+            n_server.start()
         # Run netperf with message size defined in range.
-        stress_timeout = float(params.get("netperf_test_timeout", 1200))
-        netperf_test_duration = float(params.get("netperf_test_duration", 60))
+        netperf_test_duration = int(params.get("netperf_test_duration", 60))
         netperf_para_sess = params.get("netperf_para_sessions", "1")
         test_protocol = params.get("test_protocol", "TCP_STREAM")
         netperf_cmd_prefix = params.get("netperf_cmd_prefix", "")
         test_option = "-t %s -l %s" % (test_protocol, netperf_test_duration)
         start_time = time.time()
-        stop_time = start_time + stress_timeout
+        stop_time = start_time + netperf_test_duration
+        num = 0
+        s_len = len(server_infos)
+        for n_client in netperf_clients:
+            index = num % s_len
+            server_ip = server_infos[index]["ip"]
+            n_client.bg_start(server_ip, test_option,
+                              netperf_para_sess, netperf_cmd_prefix)
+            if utils_misc.wait_for(n_client.is_netperf_running, 10, 0, 1,
+                                   "Wait netperf test start"):
+                logging.info("Netperf test start successfully.")
+            else:
+                raise error.TestError("Can not start netperf client.")
+            num += 1
+        # here when set a run flag, when other case call this case as a
+        # subprocess backgroundly, can set this run flag to False to stop
+        # the stress test.
+        env["netperf_run"] = True
 
-        netperf_client.bg_start(netserver_ip, test_option,
-                                netperf_para_sess, netperf_cmd_prefix)
-        if utils_misc.wait_for(netperf_client.is_netperf_running, 10, 0, 1,
-                               "Wait netperf test start"):
-            logging.debug("Netperf test start successfully.")
-            # here when set a run flag, when other case call this case as a
-            # subprocess backgroundly, can set this run flag to False to stop
-            # the stress test.
-            env["netperf_run"] = True
-        else:
-            raise error.TestNAError("Can not start netperf test")
-
-        while (env["netperf_run"] and time.time() < stop_time):
-            run_left_time = stop_time - time.time()
-            if netperf_client.is_netperf_running():
-                if not utils_misc.wait_for(lambda: not
-                                           netperf_client.is_netperf_running(),
-                                           run_left_time, 0, 5,
-                                           "Wait netperf test finish"):
-                    logging.debug("Stress test timeout, finish it")
-                    break
-            netperf_client.bg_start(vm.get_address(), test_option,
-                                    netperf_para_sess)
-
+        for n_client in netperf_clients:
+            if n_client.is_netperf_running():
+                left_time = stop_time - time.time()
+                utils_misc.wait_for(lambda: not
+                                    n_client.is_netperf_running(),
+                                    left_time, 0, 5,
+                                    "Wait netperf test finish %ss" % left_time)
     finally:
-        netperf_server.stop()
-        netperf_server.package.env_cleanup(True)
-        netperf_client.package.env_cleanup(True)
+        for n_server in netperf_servers:
+            if n_server:
+                n_server.stop()
+            n_server.package.env_cleanup(True)
+        for n_client in netperf_clients:
+            if n_client:
+                n_client.package.env_cleanup(True)
         env["netperf_run"] = False
         if session:
             session.close()

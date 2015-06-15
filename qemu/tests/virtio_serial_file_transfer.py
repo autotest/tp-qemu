e1c798b1fbd417ba8f1337fcafb4f341a93b617e
Xu Tian <xutian@redhat.com>
2015-06-15 10:21:38 +0800

    tp-qemu: add regular file transfer test
    Signed-off-by: Xu Tian <xutian@redhat.com>

--- qemu/tests/virtio_serial_file_transfer.py
+++ qemu/tests/virtio_serial_file_transfer.py
@@ -21,13 +21,13 @@ def run(test, params, env):
     :param params: Dictionary with the test parameters.
     :param env: Dictionary with test environment.
     """
-    testtag = params.get("subtest_tag", None)
-    subtest = params.get("subtest", "stop_continue")
+    testtag = params.get("virt_subtest_tag", None)
+    subtest = params.get("virt_subtest_type", None)
+    start_timeout = float(params.get("start_transfer_timeout", 240))
     file_sender = params.get("file_sender", "guest")
     md5_check = params.get("md5_check", "yes") == "yes"
     ports_name = params.get("file_transfer_serial_port").split()
     suppress_exception = params.get("suppress_exception", "yes") == "yes"
-    start_transfer_timeout = int(params.get("start_transfer_timeout", 120))
     transfer_timeout = int(params.get("transfer_timeout", 720))
     target = utils_test.run_virtio_serial_file_transfer
     args = (test, params, env,)
@@ -39,20 +39,21 @@ def run(test, params, env):
     transfer_thread.start()
     start_transfer = None
     start = time.time()
-    while time.time() < start + start_transfer_timeout:
+    while time.time() < start + start_timeout:
         start_transfer = env["serial_file_transfer_start"]
         if start_transfer:
             break
         time.sleep(0.5)
     if start_transfer is None:
         raise error.TestFail("Wait transfer file thread start timeout "
-                             "in %ss" % start_transfer_timeout)
+                             "in %ss" % start_timeout)
     elif start_transfer is False:
-        file_size = params["file_size"]
+        file_size = params["filesize"]
         raise error.TestError("File transfer finished before run sub test," +
-                              " Please increase 'file_size' param in cfg," +
+                              " Please increase 'filesize' param in cfg," +
                               " current value is '%s'" % file_size)
-    utils_test.run_virt_sub_test(test, params, env, subtest, testtag)
+    if subtest:
+        utils_test.run_virt_sub_test(test, params, env, subtest, testtag)
     transfer_thread.join(timeout=transfer_timeout,
                          suppress_exception=suppress_exception)
     if transfer_thread.is_alive():

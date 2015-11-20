a4bf4d8a0fc06ea6abc5e09de21eb685871f2318
Mike Cao <bcao@redhat.com>
2015-11-20 13:44:58 +0800

    fix typo
    Signed-off-by: Mike Cao <bcao@redhat.com>

--- qemu/tests/driver_load.py
+++ qemu/tests/driver_load.py
@@ -64,7 +64,7 @@ def run(test, params, env):
         time.sleep(5)
         status, output = session.cmd_status_output(driver_load_cmd)
         if params["os_type"] == "windows" and "device(s) are enabled" not in output:
-            raise error.TestError("failed to unload driver %s" % output)
+            raise error.TestError("failed to load driver %s" % output)
         logging.info("status %s load output %s"
                      % (status, output))
         time.sleep(5)

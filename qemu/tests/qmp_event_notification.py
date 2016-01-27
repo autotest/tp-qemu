db87c4529bb9b040fdca2d4e4d9b5d6491286688
Xu Han <xuhan@redhat.com>
2016-01-27 16:43:11 +0800

    qemu.tests.qmp_event_notification: allow passing options to call-back function
    Running guest cmd "shutdown -h now" would cause a termination of shell
    session, this would raise an exception that can fail the test. So add
    the ability for passing "ignore_all_errors=True" to call-back function
    which runs in guest.
    
    Signed-off-by: Xu Han <xuhan@redhat.com>

--- qemu/tests/qmp_event_notification.py
+++ qemu/tests/qmp_event_notification.py
@@ -29,26 +29,34 @@ def run(test, params, env):
     qmp_monitor = filter(lambda x: x.protocol == "qmp", vm.monitors)[0]
     humam_monitor = filter(lambda x: x.protocol == "human", vm.monitors)[0]
     callback = {"host_cmd": commands.getoutput,
-                "guest_cmd": session.get_command_output,
+                "guest_cmd": session.cmd,
                 "monitor_cmd": humam_monitor.send_args_cmd,
                 "qmp_cmd": qmp_monitor.send_args_cmd}
 
-    def send_cmd(cmd):
+    def send_cmd(cmd, options={}):
         if cmd_type in callback.keys():
-            return callback[cmd_type](cmd)
+            return callback[cmd_type](cmd, **options)
         else:
             raise error.TestError("cmd_type is not supported")
 
+    pre_event_cmd = params.get("pre_event_cmd", "")
+    pre_event_cmd_options = eval(
+        "dict({0})".format(params.get("pre_event_cmd_options", "")))
     event_cmd = params.get("event_cmd")
+    event_cmd_options = eval(
+        "dict({0})".format(params.get("event_cmd_options", "")))
+    post_event_cmd = params.get("post_event_cmd", "")
+    post_event_cmd_options = eval(
+        "dict({0})".format(params.get("post_event_cmd_options", "")))
     cmd_type = params.get("event_cmd_type")
     event_check = params.get("event_check")
     timeout = int(params.get("check_timeout", 360))
     action_check = params.get("action_check")
 
-    if params.get("pre_event_cmd"):
-        send_cmd(params.get("pre_event_cmd"))
+    if pre_event_cmd:
+        send_cmd(pre_event_cmd, pre_event_cmd_options)
 
-    send_cmd(event_cmd)
+    send_cmd(event_cmd, event_cmd_options)
 
     end_time = time.time() + timeout
     qmp_monitors = vm.get_monitors_by_type("qmp")
@@ -77,7 +85,7 @@ def run(test, params, env):
         raise error.TestFail("Did not receive qmp %s event notification"
                              % event_check)
 
-    if params.get("post_event_cmd"):
-        send_cmd(params.get("post_event_cmd"))
+    if post_event_cmd:
+        send_cmd(post_event_cmd, post_event_cmd_options)
     if session:
         session.close()

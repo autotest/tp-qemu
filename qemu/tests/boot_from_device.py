83f4326802b68e1e43d143b99ca9fb56645d3837
yanwang <yan.wang@easystack.cn>
2017-01-23 14:30:28 +0800

    fix boot_from_scsi_cdrom.with_specify_device
    The boot_menu_key is 'f12' in qemu1.5.but in qemu2.6,it becomes 'esc'.

--- qemu/tests/boot_from_device.py
+++ qemu/tests/boot_from_device.py
@@ -109,7 +109,7 @@ def run(test, params, env):
             cleanup(device_name)
 
     timeout = int(params.get("login_timeout", 360))
-    boot_menu_key = params.get("boot_menu_key", 'f12')
+    boot_menu_key = 'esc '
     boot_menu_hint = params.get("boot_menu_hint")
     boot_fail_info = params.get("boot_fail_info")
     boot_device = params.get("boot_device")

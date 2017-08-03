1b5cc3ce0c4a3e0fac89fd91925afb27d5416a37
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2017-08-03 17:27:20 +0530

    Added support to run avocado tests inside guest
    Added test to support avocado tests run inside guest
    
    Single or multiple avocado tests can be run by single
    invocation of this test.
    
    Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

---
+++ generic/tests/avocado_guest.py
@@ -0,0 +1,32 @@
+from virttest import utils_test
+
+
+def run(test, params, env):
+    """
+    Run an avocado test inside a guest.
+
+    :param test: QEMU test object.
+    :param params: Dictionary with test parameters.
+    :param env: Dictionary with the test environment.
+    """
+    vm = env.get_vm(params["main_vm"])
+    vm.verify_alive()
+    timeout = int(params.get("test_timeout", 3600))
+    testlist = []
+    avocadoinstalltype = params.get("avocadoinstalltype", "pip")
+    avocadotestrepo = params.get("avocadotestrepo",
+                                 "https://github.com/avocado-framework-tests/avocado-misc-tests.git")
+    avocadotest = params.get("avocadotest", "cpu/ebizzy.py")
+    avocadomux = params.get("avocadomux", "")
+    avocadotestargs = params.get("avocadotestargs", "")
+    for index, item in enumerate(avocadotest.split(',')):
+        try:
+            mux = ''
+            mux = avocadomux.split(',')[index]
+        except IndexError:
+            pass
+        testlist.append((item, mux))
+    utils_test.run_avocado(vm, params, test, testlist, timeout=timeout,
+                           testrepo=avocadotestrepo,
+                           installtype=avocadoinstalltype, reinstall=False,
+                           add_args=avocadotestargs, ignore_result=False)

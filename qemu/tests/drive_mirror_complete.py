4a709ecbecf76dccca0e7728156fd6cae64410b5
qizhu <qizhu@redhat.com>
2018-01-30 17:16:59 +0800

    drive_mirror: Add test case for data integrity check
    Simply by create files before mirror start and verify files after
    reopen. Need to move action_after_reopen() after vm.resume() for
    it, but it won't affect other cases.
    
    Signed-off-by: qizhu <qizhu@redhat.com>

--- qemu/tests/drive_mirror_complete.py
+++ qemu/tests/drive_mirror_complete.py
@@ -29,17 +29,18 @@ def run(test, params, env):
     try:
         source_image = mirror_test.get_image_file()
         target_image = mirror_test.get_target_image()
+        mirror_test.action_before_start()
         mirror_test.start()
         mirror_test.action_when_steady()
         mirror_test.vm.pause()
         mirror_test.reopen()
-        mirror_test.action_after_reopen()
         device_id = mirror_test.vm.get_block({"file": target_image})
         if device_id != mirror_test.device:
             test.error("Mirrored image not being used by guest")
         error_context.context("Compare fully mirrored images", logging.info)
         qemu_img.compare_images(source_image, target_image, force_share=True)
         mirror_test.vm.resume()
+        mirror_test.action_after_reopen()
         if params.get("boot_target_image", "no") == "yes":
             mirror_test.vm.destroy()
             params = params.object_params(tag)

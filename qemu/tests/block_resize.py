aeef93768d4300168f10a95575558b5a8d22584f
Peixiu Hou <phou@redhat.com>
2018-03-23 15:13:52 +0800

    block_resize: Update block resize test
    1. Add resize linux file system and partition steps.
    2. Add iozone test after resize.
    3. Add resize test on windows system disk.
    4. Move many codes to avocado-vt functions,
       like get_disk_size() and extend_volume(), shrink_volume(),
       they all were merged.
    
    Signed-off-by: Peixiu Hou <phou@redhat.com>

--- qemu/tests/block_resize.py
+++ qemu/tests/block_resize.py
@@ -1,11 +1,17 @@
 import logging
-import re
+import json
 
+from avocado.utils import wait
 from virttest import error_context
-from virttest import utils_misc
+from virttest import utils_numeric
 from virttest import utils_test
+from virttest import utils_disk
 from virttest import storage
 from virttest import data_dir
+from virttest.utils_windows import drive
+from virttest.qemu_storage import QemuImg
+
+from provider.storage_benchmark import generate_instance
 
 
 @error_context.context_aware
@@ -13,159 +19,132 @@ def run(test, params, env):
     """
     KVM block resize test:
 
-    1) Start guest with data image and check the data image size.
-    2) Enlarge(or Decrease) the data image and check it in guest.
+    1) Start guest with data disk or system disk.
+    2) Do format disk in guest if needed.
+    3) Enlarge the data disk image from qemu monitor.
+    4) Extend data disk partition/file-system in guest.
+    5) Verify the data disk size match expected size.
+    6) Reboot the guest.
+    7) Do iozone test.
+    8) Shrink data disk partition/file-system in guest.
+    9) Shrink data disk image from qemu monitor.
+    10) Verify the data disk size match expected size.
+    11) Reboot the guest.
+    12) Do iozone test.
 
     :param test: QEMU test object
     :param params: Dictionary with the test parameters
     :param env: Dictionary with test environment.
     """
-    def get_block_size(session, block_cmd, block_pattern):
+    def verify_disk_size(session, os_type, disk):
         """
-        Get block size inside guest.
-        """
-        output = session.cmd_output(block_cmd)
-        block_size = re.findall(block_pattern, output)
-        if block_size:
-            if not re.search("[a-zA-Z]", block_size[0]):
-                return int(block_size[0])
-            else:
-                return float(utils_misc.normalize_data_size(block_size[0],
-                                                            order_magnitude="B"))
-        else:
-            test.error("Can not find the block size for the"
-                       " deivce. The output of command"
-                       " is: %s" % output)
-
-    def compare_block_size(session, block_cmd, block_pattern):
-        """
-        Compare the current block size with the expected size.
+        Verify the current block size match with the expected size.
         """
         global current_size
-        current_size = get_block_size(session,
-                                      block_size_cmd, block_size_pattern)
+        current_size = utils_disk.get_disk_size(session, os_type, disk)
+        accept_ratio = float(params.get("accept_ratio", 0))
         if (current_size <= block_size and
                 current_size >= block_size * (1 - accept_ratio)):
             logging.info("Block Resizing Finished !!! \n"
                          "Current size %s is same as the expected %s",
                          current_size, block_size)
             return True
-        return
 
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     timeout = float(params.get("login_timeout", 240))
     driver_name = params.get("driver_name")
-    session = vm.wait_for_login(timeout=timeout)
-
-    if params.get("os_type") == "windows" and driver_name:
-        session = utils_test.qemu.windrv_check_running_verifier(session, vm,
-                                                                test, driver_name,
-                                                                timeout)
+    os_type = params["os_type"]
+    fstype = params.get("fstype")
+    labeltype = params.get("labeltype", "msdos")
+    img_size = params.get("image_size_stg", "10G")
+    mpoint = params.get("disk_letter", "C")
+    disk = params.get("disk_index", 0)
     data_image = params.get("images").split()[-1]
     data_image_params = params.object_params(data_image)
-    data_image_size = data_image_params.get("image_size")
-    data_image_size = float(utils_misc.normalize_data_size(data_image_size,
-                                                           order_magnitude="B"))
     data_image_filename = storage.get_image_filename(data_image_params,
                                                      data_dir.get_data_dir())
     data_image_dev = vm.get_block({'file': data_image_filename})
+    img = QemuImg(data_image_params, data_dir.get_data_dir(), data_image)
+    block_virtual_size = json.loads(img.info(force_share=True,
+                                    output="json"))["virtual-size"]
 
-    drive_path = ""
-    if params.get("os_type") == 'linux':
-        pattern = r"(serial|wwn)=(\w+)"
-        match = re.search(pattern, params["blk_extra_params_%s"
-                          % data_image], re.M)
-        if match:
-            drive_id = match.group(2)
+    session = vm.wait_for_login(timeout=timeout)
+
+    if os_type == 'windows' and driver_name:
+        session = utils_test.qemu.windrv_check_running_verifier(session, vm,
+                                                                test, driver_name,
+                                                                timeout)
+
+    if params.get("format_disk") == "yes":
+        if os_type == 'linux':
+            disk = sorted(utils_disk.get_linux_disks(session).keys())[0]
         else:
-            test.fail("No available tag to get drive id")
-        drive_path = utils_misc.get_linux_drive_path(session, drive_id)
-        if not drive_path:
-            test.error("Failed to get '%s' drive path" % data_image)
+            disk = utils_disk.get_windows_disks_index(session, img_size)[0]
+            utils_disk.update_windows_disk_attributes(session, disk)
+        error_context.context("Formatting disk", logging.info)
+        mpoint = utils_disk.configure_empty_disk(session, disk, img_size,
+                                                 os_type, fstype=fstype,
+                                                 labeltype=labeltype)[0]
+        partition = mpoint.replace('mnt', 'dev') if 'mnt' in mpoint else None
 
-    block_size_cmd = params["block_size_cmd"].format(drive_path)
-    block_size_pattern = params.get("block_size_pattern")
-    need_reboot = params.get("need_reboot", "no") == "yes"
-    accept_ratio = float(params.get("accept_ratio", 0))
-
-    error_context.context("Check image size in guest", logging.info)
-    block_size = get_block_size(session, block_size_cmd, block_size_pattern)
-    if (block_size > data_image_size or
-            block_size < data_image_size * (1 - accept_ratio)):
-        test.error("Image size from guest and image not match"
-                   "Block size get from guest: %s \n"
-                   "Image size get from image: %s \n"
-                   % (block_size, data_image_size))
-
-    if params.get("guest_prepare_cmd"):
-        session.cmd(params.get("guest_prepare_cmd"))
-
-    if params.get("format_disk", "no") == "yes":
-        error_context.context("Format disk", logging.info)
-        utils_misc.format_windows_disk(session, params["disk_index"],
-                                       mountpoint=params["disk_letter"])
-
-    disk_update_cmd = params.get("disk_update_cmd")
-    if disk_update_cmd:
-        disk_update_cmd = disk_update_cmd.split("::")
-
-    disk_rescan_cmd = params.get("disk_rescan_cmd")
-
-    block_size = data_image_size
-    disk_change_ratio = params["disk_change_ratio"]
-    for index, ratio in enumerate(disk_change_ratio.strip().split()):
-        old_block_size = block_size
-        block_size = int(int(data_image_size) * float(ratio))
-
-        if block_size == old_block_size:
-            logging.warn("Block size is not changed in round %s."
-                         " Just skip it" % index)
-            continue
-
-        if disk_update_cmd:
-            if "DISK_CHANGE_SIZE" in disk_update_cmd[index]:
-                disk_unit = params.get("disk_unit", "M")
-                size = abs(block_size - old_block_size)
-                change_size = utils_misc.normalize_data_size("%sB" % size,
-                                                             disk_unit)
-                disk_update_cmd[index] = re.sub("DISK_CHANGE_SIZE",
-                                                change_size.split(".")[0],
-                                                disk_update_cmd[index])
-
-        # So far only virtio drivers support online auto block size change in
-        # linux guest. So we need manully update the the disk or even reboot
-        # guest to get the right block size after change it from monitor.
+    for ratio in params.objects("disk_change_ratio"):
+        block_size = int(int(block_virtual_size) * float(ratio))
 
         # We need shrink the disk in guest first, than in monitor
-        if block_size < old_block_size and disk_update_cmd:
+        if float(ratio) < 1.0:
             error_context.context("Shrink disk size to %s in guest"
                                   % block_size, logging.info)
-            session.cmd(disk_update_cmd[index])
+            if os_type == 'windows':
+                shr_size = utils_numeric.normalize_data_size(str(
+                    utils_disk.get_disk_size(session, os_type, disk) -
+                    block_size), 'M').split(".")[0]
+                drive.shrink_volume(session, mpoint, shr_size)
+            else:
+                utils_disk.resize_filesystem_linux(session, partition, str(block_size))
+                utils_disk.resize_partition_linux(session, partition, str(block_size))
 
         error_context.context("Change disk size to %s in monitor"
                               % block_size, logging.info)
         vm.monitor.block_resize(data_image_dev, block_size)
 
-        if need_reboot:
+        if params.get("guest_prepare_cmd", ""):
+            session.cmd(params.get("guest_prepare_cmd"))
+        if params.get("need_reboot") == "yes":
             session = vm.reboot(session=session)
-        elif disk_rescan_cmd:
-            error_context.context("Rescan disk", logging.info)
-            session.cmd(disk_rescan_cmd)
+        if params.get("need_rescan") == "yes":
+            drive.rescan_disks(session)
 
-        # We need expand disk in monitor first than extend it in guest
-        if block_size > old_block_size and disk_update_cmd:
+        # We need extend disk in monitor first than extend it in guest
+        if float(ratio) > 1.0:
             error_context.context("Extend disk to %s in guest"
                                   % block_size, logging.info)
-            session.cmd(disk_update_cmd[index])
-
+            if os_type == 'windows':
+                drive.extend_volume(session, mpoint)
+            else:
+                utils_disk.resize_partition_linux(session, partition, str(block_size))
+                utils_disk.resize_filesystem_linux(session, partition,
+                                                   utils_disk.SIZE_AVAILABLE)
         global current_size
         current_size = 0
-        if not utils_misc.wait_for(lambda: compare_block_size
-                                   (session, block_size_cmd,
-                                    block_size_pattern),
-                                   20, 0, 1, "Block Resizing"):
-            test.fail("Block size get from guest is not"
-                      "the same as expected \n"
-                      "Reported: %s\n"
-                      "Expect: %s\n" % (current_size, block_size))
+        if not wait.wait_for(lambda: verify_disk_size(session, os_type,
+                             disk), 20, 0, 1, "Block Resizing"):
+            test.fail("Block size get from guest is not same as expected.\n"
+                      "Reported: %s\nExpect: %s\n" % (current_size, block_size))
+        session = vm.reboot(session=session)
+
+        if os_type == 'linux':
+            if not utils_disk.is_mount(partition, dst=mpoint,
+                                       fstype=fstype, session=session):
+                utils_disk.mount(partition, mpoint,
+                                 fstype=fstype, session=session)
+
+        if params.get('iozone_test') == 'yes':
+            iozone_timeout = float(params.get("iozone_timeout", 1800))
+            iozone_cmd_options = params.get("iozone_option") % mpoint
+            io_test = generate_instance(params, vm, 'iozone')
+            try:
+                io_test.run(iozone_cmd_options, iozone_timeout)
+            finally:
+                io_test.clean()
+    session.close()

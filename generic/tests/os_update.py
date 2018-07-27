c6af331074717ce347d7a69568ebd0107f42ce73
Balamuruhan S <bala24@linux.vnet.ibm.com>
2018-07-27 17:57:25 +0530

    generic/tests/os_update: refactor and have MultiVM support
    yum_update.py had support to perform os update for RHEL based guests,
    refactor it to support Ubuntu and adopt MultiVM. The patch also renames
    yum_update.py -> os_update.py similarly cfg file is renamed.
    
    Signed-off-by: Balamuruhan S <bala24@linux.vnet.ibm.com>

---
+++ generic/tests/os_update.py
@@ -0,0 +1,25 @@
+import logging
+
+
+def run(test, params, env):
+    """
+    Runs yum update and yum update kernel on RHEL or apt
+    update and apt upgrade on Ubuntu based multi VM environment.
+
+    :param test: kvm test object.
+    :param params: Dictionary with test parameters.
+    :param env: Dictionary with the test environment.
+    """
+    vms = env.get_all_vms()
+    timeout = int(params.get("os_update_timeout"))
+    for vm in vms:
+        if not vm.is_alive():
+            vm.start()
+        cmd = "yum update -y"
+        if "ubuntu" in vm.get_distro().lower():
+            cmd = "apt update && apt upgrade -y"
+        session = vm.wait_for_login()
+        logging.debug("Performing %s on VM %s", cmd, vm.name)
+        if session.cmd_status(cmd, timeout=timeout) != 0:
+            test.fail("Failed to update VM %s using %s" % (vm.name, cmd))
+        session.close()

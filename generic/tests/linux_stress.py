76c2f8faba868245a1d9811689525db63072d0f2
Srikanth Aithal <sraithal@linux.vnet.ibm.com>
2018-08-24 18:31:58 +0530

    Extend linux stress to support multivm with multi stress tools
    Existing linux stress was supporting single vm with single stress
    tool. This commit extends that testcase to now support multiple
    guests running same/different stress tools inside for given duration
    
    Signed-off-by: Srikanth Aithal <sraithal@linux.vnet.ibm.com>

--- generic/tests/linux_stress.py
+++ generic/tests/linux_stress.py
@@ -1,4 +1,7 @@
 import time
+import logging
+
+from avocado.core import exceptions
 
 from virttest import utils_test
 
@@ -19,19 +22,47 @@ def run(test, params, env):
     :param env: Dictionary with test environment
     """
 
-    vm = env.get_vm(params['main_vm'])
-    vm.verify_alive()
-    stress = utils_test.VMStress(vm, 'stress', params)
-    stress.load_stress_tool()
-    stress_duration = int(params.get('stress_duration', 0))
+    stress_duration = int(params.get("stress_duration", "0"))
     # NOTE: stress_duration = 0 ONLY for some legacy test cases using
     # autotest stress.control as their sub test.
     # Please DO define stress_duration to make sure the clean action
     # being performed, if your case can not be controlled by time,
     # use utils_test.VMStress() directly
+    stress_type = params.get("stress_type", "stress")
+    vms = env.get_all_vms()
+    up_time = {}
+    error = False
+    stress_server = {}
+
+    for vm in vms:
+        try:
+            up_time[vm.name] = vm.uptime()
+            stress_server[vm.name] = utils_test.VMStress(vm, stress_type, params)
+            stress_server[vm.name].load_stress_tool()
+        except exceptions.TestError as err_msg:
+            error = True
+            logging.error(err_msg)
 
     if stress_duration:
         time.sleep(stress_duration)
-        stress.unload_stress()
-        stress.clean()
-        vm.verify_kernel_crash()
+
+    for vm in vms:
+        try:
+            s_ping, o_ping = utils_test.ping(vm.get_address(), count=5, timeout=20)
+            if s_ping != 0:
+                error = True
+                logging.error("%s seem to have gone out of network", vm.name)
+                continue
+            uptime = vm.uptime()
+            if up_time[vm.name] > uptime:
+                error = True
+                logging.error("%s seem to have rebooted during the stress run", vm.name)
+            stress_server[vm.name].unload_stress()
+            stress_server[vm.name].clean()
+            vm.verify_dmesg()
+        except exceptions.TestError as err_msg:
+            error = True
+            logging.error(err_msg)
+
+    if error:
+        test.fail("Run failed: see error messages above")

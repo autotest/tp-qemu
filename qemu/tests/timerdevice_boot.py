31e616364ada73281bd9a594eb2fda796588c810
Sitong Liu <siliu@redhat.com>
2018-11-26 21:58:09 +0800

    timerdevice.boot: add reboot test case for rtc_base="2020-06-17"
    Signed-off-by: Sitong Liu <siliu@redhat.com>

--- qemu/tests/timerdevice_boot.py
+++ qemu/tests/timerdevice_boot.py
@@ -19,7 +19,7 @@ def run(test, params, env):
     """
     Timer device boot guest:
 
-    1) Check host clock's sync status with chronyd
+    1) Stop chronyd and sync host clock's time with ntp server
     2) Add some load on host (Optional)
     3) Boot the guest with specific clock source
     4) Check the clock source currently used on guest
@@ -59,12 +59,13 @@ def run(test, params, env):
             raise err
         return guest_time
 
-    def verify_timedrift(session, is_hardware=False):
+    def verify_timedrift(session, is_hardware=False, base_time=None):
         """
-        Verify timedrift between host and guest.
+        Verify timedrift between two targets. ex. host and guest.
 
         :param session: VM session.
         :param is_hardware: if need to verify guest's hardware time.
+        :param base_time: base time in qemu command, ex. "2006-06-17"
         """
         # Command to run to get the current time
         time_command = params["time_command"]
@@ -75,7 +76,7 @@ def run(test, params, env):
         timerdevice_drift_threshold = float(params.get(
             "timerdevice_drift_threshold", 3))
 
-        time_type = "system" if not is_hardware else "harware"
+        time_type = "system" if not is_hardware else "hardware"
         error_context.context("Check the %s time on guest" % time_type,
                               logging.info)
         host_time, guest_time = utils_test.get_time(session, time_command,
@@ -83,11 +84,31 @@ def run(test, params, env):
                                                     time_format)
         if is_hardware:
             guest_time = get_hwtime(session)
-        drift = abs(float(host_time) - float(guest_time))
-        if drift > timerdevice_drift_threshold:
-            test.fail("The guest's %s time is different with"
-                      " host's system time. Host time: '%s', guest time:"
-                      " '%s'" % (time_type, host_time, guest_time))
+        if not base_time:
+            drift = abs(float(host_time) - float(guest_time))
+            if drift > timerdevice_drift_threshold:
+                test.fail("The guest's %s time is different with"
+                          " host's system time. Host time: '%s', guest time:"
+                          " '%s'" % (time_type, host_time, guest_time))
+        else:
+            base_pattern = "%Y-%m-%dT%H:%M:%S"
+            try:
+                time_struct = time.strptime(base_time, base_pattern)
+            except ValueError:
+                logging.info("Add default time to base date: %s" % base_time)
+                if params["os_type"] == "windows":
+                    base_time += "T00:00:00"
+                else:
+                    base_time += "T08:00:00"
+                try:
+                    time_struct = time.strptime(base_time, base_pattern)
+                except:
+                    raise
+            target_time = time.mktime(time_struct)
+            # assume boot time will not large than 3 minutes
+            if (guest_time - target_time) > 180:
+                test.fail("The guest system time is not from base time "
+                          "%s" % base_time)
 
     def get_current_clksrc(session):
         cmd = "cat /sys/devices/system/clocksource/"
@@ -125,6 +146,7 @@ def run(test, params, env):
             test.error("please check the clocksource you want to set, "
                        "it's not supported by current guest, current "
                        "available clocksources: %s" % avail_clksrc)
+    base_time = params.get("rtc_base")
 
     error_context.context("sync host time with NTP server",
                           logging.info)
@@ -165,9 +187,11 @@ def run(test, params, env):
 
     error_context.context("check timedrift between guest and host.",
                           logging.info)
-    verify_timedrift(session)
+    if base_time in ("utc", "localtime"):
+        base_time = None
+    verify_timedrift(session, base_time=base_time)
     if params["os_type"] == "linux":
-        verify_timedrift(session, is_hardware=True)
+        verify_timedrift(session, is_hardware=True, base_time=base_time)
 
     if params.get("timerdevice_reboot_test") == "yes":
         sleep_time = params.get("timerdevice_sleep_time")
@@ -180,9 +204,9 @@ def run(test, params, env):
         error_context.context("Check timedrift between guest and host "
                               "after reboot.", logging.info)
         vm.reboot(timeout=timeout, serial=True)
-        verify_timedrift(session)
+        verify_timedrift(session, base_time=base_time)
         if params["os_type"] == "linux":
-            verify_timedrift(session, is_hardware=True)
+            verify_timedrift(session, is_hardware=True, base_time=base_time)
     if need_restore_clksrc:
         update_clksrc(session, origin_clksrc)
     session.close()

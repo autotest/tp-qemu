371d0fd9006cedeebfb740826f0976f71212507a
Yu Wang <wyu@redhat.com>
2019-06-10 12:00:31 +0800

    Add new case for netkvm_cpu_mapping
    Check traceview log and get correct cpu mapping infomation
    
    Signed-off-by: Yu Wang <wyu@redhat.com>

---
+++ qemu/tests/netkvm_cpu_mapping.py
@@ -0,0 +1,35 @@
+import logging
+import re
+
+from virttest import error_context
+from virttest import utils_net
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Get cpu mapping info from traceview session, and check
+    whether it matches the vectors count.
+
+    :params test: the test object
+    :params params: the test params
+    :params env: test environment
+
+    """
+
+    # boot the vm with the  queues
+    queues = int(params["queues"])
+    error_context.context("Boot the guest with queues = %s" % queues,
+                          logging.info)
+
+    vm = env.get_vm(params["main_vm"])
+    vm.verify_alive()
+    nic = vm.virtnet[0]
+    nic_vectors = int(nic['vectors']) if nic['vectors'] else (2 * queues + 2)
+    error_context.context("Get CPU mapping info by traceview", logging.info)
+    output = utils_net.dump_traceview_log(params, vm)
+    check_reg = "SetupInterrruptAffinity.*?Option = 0x0"
+    mapping_count = len(re.findall(check_reg, output))
+    if mapping_count != nic_vectors:
+        test.fail("Mapping info count %s not match vectors %s" %
+                  (mapping_count, nic_vectors))

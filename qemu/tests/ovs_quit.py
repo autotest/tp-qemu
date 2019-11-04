134f1d731b60037905b0de65a37d99eda2b18129
Yihuang Yu <yihyu@redhat.com>
2019-11-04 18:49:05 +0800

    ovs_quit: Fix bugs and improve code
    This test case does not work now, fix bugs and improve code
    
    Signed-off-by: Yihuang Yu <yihyu@redhat.com>

--- qemu/tests/ovs_quit.py
+++ qemu/tests/ovs_quit.py
@@ -1,11 +1,9 @@
-from avocado.utils import process
-from avocado.utils import path as utils_path
-
 from virttest import data_dir
 from virttest import env_process
 from virttest import error_context
 from virttest import virt_vm
 from virttest import utils_misc
+from virttest import utils_net
 
 
 @error_context.context_aware
@@ -22,15 +20,11 @@ def run(test, params, env):
     :param env: Dictionary with test environment.
     """
 
-    def get_ovs_ports(ovs):
-        cmd = "ovs-vsctl list-ports %s" % ovs
-        return set(process.system_output(cmd).splitlines())
-
-    utils_path.find_command("ovs-vsctl")
     netdst = params.get("netdst")
-    if netdst not in process.system_output("ovs-vsctl list-br"):
-        test.error("%s isn't is openvswith bridge" % netdst)
+    if not utils_net.ovs_br_exists(netdst):
+        test.cancel("%s isn't an openvswith bridge" % netdst)
 
+    host_bridge = utils_net.find_bridge_manager(netdst)
     deps_dir = data_dir.get_deps_dir("ovs")
     nic_script = utils_misc.get_path(deps_dir, params["nic_script"])
     nic_downscript = utils_misc.get_path(deps_dir, params["nic_downscript"])
@@ -42,15 +36,15 @@ def run(test, params, env):
     params["nettype"] = "bridge"
     params["nic_model"] = "virtio-net-pci"
 
+    ports = set(host_bridge.list_ports(netdst))
     try:
-        ports = get_ovs_ports(netdst)
         env_process.preprocess_vm(test, params, env, params["main_vm"])
         env.get_vm(params["main_vm"])
-    except virt_vm.VMStartError:
-        ports = get_ovs_ports(netdst) - ports
+    except virt_vm.VMCreateError:
+        ports = set(host_bridge.list_ports(netdst)) - ports
         if ports:
             for p in ports:
-                process.system("ovs-vsctl del-if %s %s" % (netdst, p))
+                host_bridge.del_port(netdst, p)
             test.fail("%s not delete after qemu quit." % ports)
     else:
         test.fail("Qemu should quit with error")

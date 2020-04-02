f85da5e3ab1ffd5352301559b8ee82493076e458
Lukáš Doktor <ldoktor@redhat.com>
2020-04-02 19:33:44 +0200

    qemu.migration_with_speed_measurement: Fix cpu_flags location
    For some time the "cpu_flags" test location had been adjusted to include
    "src" dir in "provider/cpuflags.py", let's reflect this change in this
    test as well.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- qemu/tests/migration_with_speed_measurement.py
+++ qemu/tests/migration_with_speed_measurement.py
@@ -129,7 +129,7 @@ def run(test, params, env):
         vm.monitor.migrate_set_speed(mig_speed)
 
         cmd = ("%s/cpuflags-test --stressmem %d,%d" %
-               (os.path.join(install_path, "cpu_flags"),
+               (os.path.join(install_path, "cpu_flags", "src"),
                 vm_mem * 4, vm_mem / 2))
         logging.debug("Sending command: %s" % (cmd))
         session.sendline(cmd)

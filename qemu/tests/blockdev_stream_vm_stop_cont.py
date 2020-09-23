0689892e034f7d43e6d4ee93d27c954dd17545ef
zhencliu <zhencliu@redhat.com>
2020-09-23 11:58:53 +0800

    blockdev_stream_vm_stop_cont: Add stream test with vm stop/cont
      Do block-stream while vm stop/cont
    
    Signed-off-by: Zhenchao Liu <zhencliu@redhat.com>

---
+++ qemu/tests/blockdev_stream_vm_stop_cont.py
@@ -0,0 +1,42 @@
+import time
+import random
+
+from provider.blockdev_stream_parallel import BlockdevStreamParallelTest
+
+
+class BlockdevStreamVMStopContTest(BlockdevStreamParallelTest):
+    """do block-stream and vm stop/cont in parallel"""
+
+    def stop_cont_vm(self):
+        """Stop VM for a while, then resume it"""
+        self.main_vm.pause()
+        t = int(random.choice(self.params.objects('vm_stop_time_list')))
+        time.sleep(t)
+        self.main_vm.resume()
+
+    def do_test(self):
+        super(BlockdevStreamVMStopContTest, self).do_test()
+        self.clone_vm.destroy()
+        self.remove_files_from_system_image()
+
+
+def run(test, params, env):
+    """
+    Basic block stream test during vm stop and cont
+
+    test steps:
+        1. boot VM
+        2. add a snapshot image for the system disk
+        3. create a file on system disk
+        4. take snapshot for system disk
+        5. create another file
+        6. do block-stream(system->snapshot) and vm stop/continue in parallel
+        7. restart VM with the snapshot disk, check both files and md5sum
+        8. remove testing files from system image
+
+    :param test: test object
+    :param params: test configuration dict
+    :param env: env object
+    """
+    stream_test = BlockdevStreamVMStopContTest(test, params, env)
+    stream_test.run_test()

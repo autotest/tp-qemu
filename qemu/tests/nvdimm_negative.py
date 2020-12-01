3b28c056997182dc56e396f71141b903008cbd56
Yumei Huang <yuhuang@redhat.com>
2020-12-01 14:33:36 +0800

    nvdimm_negative: Add case to test negative align option
    Signed-off-by: Yumei Huang <yuhuang@redhat.com>

---
+++ qemu/tests/nvdimm_negative.py
@@ -0,0 +1,32 @@
+import logging
+import re
+
+from virttest import error_context
+from virttest import virt_vm
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Nvdimm option align negative test:
+    1) Boot vm with incorrect align options
+    2) Check if can get the expected qemu output
+
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+
+    vm = env.get_vm(params["main_vm"])
+    params['start_vm'] = 'yes'
+    error_msg = params.get('error_msg', '')
+    try:
+        vm.create(params=params)
+        output = vm.process.get_output()
+    except virt_vm.VMCreateError as e:
+        output = str(e)
+
+    error_context.context("Check the expected error message: %s"
+                          % error_msg, logging.info)
+    if not re.search(error_msg, output):
+        test.fail("Can not get expected error message: %s" % error_msg)

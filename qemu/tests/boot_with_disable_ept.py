d78b6a5981a7a3786839b76cae4351ab63591184
Leidong Wang <leidwang@redhat.com>
2020-12-08 12:19:37 +0800

    boot_with_disable_ept:boot guest after disable ept/npt
    Signed-off-by: Leidong Wang <leidwang@redhat.com>

---
+++ qemu/tests/boot_with_disable_ept.py
@@ -0,0 +1,24 @@
+from avocado.utils import process
+
+from virttest import env_process
+
+
+def run(test, params, env):
+    """
+    Boot guest after disable ept/npt:
+    1) Disable ept/npt
+    2) Boot up guest
+    3) Destory the guest and restore ept/npt
+
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment
+    """
+    output = process.getoutput(params["check_status_cmd"])
+    if output != params["expected_status"]:
+        test.fail("Disable %s failed" % params["parameter_name"])
+    params["start_vm"] = 'yes'
+    env_process.preprocess_vm(test, params, env, params["main_vm"])
+    vm = env.get_vm(params['main_vm'])
+    session = vm.wait_for_login()
+    session.close()

07ed2101f195e4c17862e5fb33f3359a2dc9fff4
nanliu <nanliu@redhat.com>
2020-12-23 14:49:11 +0800

    chardev:test native TLS encryption on chardev TCP transports
    Signed-off-by: Nana Liu <nanliu@redhat.com>

---
+++ provider/chardev_utils.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+
+from avocado.utils import process
+from virttest import utils_net
+
+from virttest.utils_conn import build_server_key, build_CA, build_client_key
+
+
+def setup_certs(params):
+    """
+    Generating certificates
+    :param params:params
+    """
+    # Create tmp certificates dir
+    cert_dir = params["cert_dir"]
+    # Remove certificates dir.
+    if os.path.exists(cert_dir):
+        shutil.rmtree(cert_dir)
+
+    # Setup the certificate authority.
+    hostname = process.run('hostname', ignore_status=False,
+                           shell=True, verbose=True).stdout_text.strip()
+    server_ip = utils_net.get_host_ip_address()
+    cn = hostname
+    ca_credential_dict = {}
+    ca_credential_dict['cakey'] = 'ca-key.pem'
+    ca_credential_dict['cacert'] = 'ca-cert.pem'
+    if not os.path.exists(cert_dir):
+        os.makedirs(cert_dir)
+    build_CA(cert_dir, cn, certtool="certtool",
+             credential_dict=ca_credential_dict)
+
+    # Setup server certificates
+    server_credential_dict = {}
+    server_credential_dict['cakey'] = 'ca-key.pem'
+    server_credential_dict['cacert'] = 'ca-cert.pem'
+    server_credential_dict['serverkey'] = 'server-key.pem'
+    server_credential_dict['servercert'] = 'server-cert.pem'
+    server_credential_dict['ca_cakey_path'] = cert_dir
+    # Build a server key.
+    build_server_key(cert_dir, cn, server_ip, certtool="certtool",
+                     credential_dict=server_credential_dict, on_local=True)
+
+    # Setup client certificates
+    client_credential_dict = {}
+    client_credential_dict['cakey'] = 'ca-key.pem'
+    client_credential_dict['cacert'] = 'ca-cert.pem'
+    client_credential_dict['clientkey'] = 'client-key.pem'
+    client_credential_dict['clientcert'] = 'client-cert.pem'
+    server_credential_dict['ca_cakey_path'] = cert_dir
+
+    # build a client key.
+    build_client_key(cert_dir,
+                     client_cn=cn, certtool="certtool",
+                     credential_dict=client_credential_dict)
---
+++ qemu/tests/chardev_tls_encryption.py
@@ -0,0 +1,108 @@
+import aexpect
+import logging
+
+from avocado.utils import process
+
+from virttest import utils_misc
+from virttest import env_process
+from virttest import error_context
+
+from provider.chardev_utils import setup_certs
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Test native TLS encryption on chardev TCP transports
+    Scenario 1:
+        a. Run gnutls server
+        b. Launch QEMU with a serial port as TLS client
+        c. Check the server endpoint output
+    Scenario 2:
+        a. Launch QEMU with a serial port as TLS server
+        b. Run gnutls client to connect TLS server
+        c. Check the client endpoint output
+    Scenario 3:
+        a. Launch QEMU with a serial port as TLS server
+        b. Execute 'cat /dev/ttyS0' in guest which boot from step 1
+        c. Launch QEMU with a serial port as TLS client
+        d. Check the output of step b
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+    clean_cmd = params["clean_cmd"]
+    try:
+        setup_certs(params)
+        expected_msg = params["expected_msg"]
+        hostname = process.run('hostname', ignore_status=False, shell=True,
+                               verbose=True).stdout_text.strip()
+        port = str(utils_misc.find_free_ports(5000, 9999, 1, hostname)[0])
+
+        # Scenario 1
+        gnutls_cmd_server = params.get("gnutls_cmd_server")
+        if gnutls_cmd_server:
+            gnutls_cmd_server = gnutls_cmd_server % port
+            params["extra_params"] = params["extra_params"] % (hostname, port)
+            error_context.context("Run gnutls server ...", logging.info)
+            tls_server = aexpect.run_bg(gnutls_cmd_server)
+            params['start_vm'] = 'yes'
+            vm_name = params['main_vm']
+            error_context.context(
+                "Launch QEMU with a serial port as TLS client", logging.info)
+            env_process.preprocess_vm(test, params, env, vm_name)
+            if not utils_misc.wait_for(
+                    lambda: expected_msg in tls_server.get_output(),
+                    first=5, timeout=15):
+                test.fail("TLS server can't connect client succssfully.")
+
+        # Scenario 2
+        gnutls_cmd_client = params.get("gnutls_cmd_client")
+        if gnutls_cmd_client:
+            gnutls_cmd_client = gnutls_cmd_client % (port, hostname)
+            params["extra_params"] = params["extra_params"] % (hostname, port)
+            params['start_vm'] = 'yes'
+            vm_name = params['main_vm']
+            error_context.context(
+                "Launch QEMU with a serial port as TLS server", logging.info)
+            env_process.preprocess_vm(test, params, env, vm_name)
+            error_context.context(
+                "Run gnutls client to connect TLS server", logging.info)
+            tls_client = aexpect.run_bg(gnutls_cmd_client)
+            if not utils_misc.wait_for(
+                    lambda: expected_msg in tls_client.get_output(),
+                    first=5, timeout=15):
+                test.fail("TLS client can't connect server succssfully.")
+
+        # Scenario 3:
+        guest_cmd = params.get("guest_cmd")
+        if guest_cmd:
+            params["start_vm"] = "yes"
+            vms = params.get("vms").split()
+            params["extra_params"] = params["extra_params_%s"
+                                            % vms[0]] % (hostname, port)
+            error_context.context(
+                "Launch QEMU with a serial port as TLS server", logging.info)
+            env_process.preprocess_vm(test, params, env, vms[0])
+            vm1 = env.get_vm(vms[0])
+            session_vm1 = vm1.wait_for_login()
+            session_vm1.cmd(guest_cmd)
+            params["extra_params"] = params["extra_params_%s"
+                                            % vms[1]] % (hostname, port)
+            error_context.context(
+                "Launch QEMU with a serial port as TLS client", logging.info)
+            env_process.preprocess_vm(test, params, env, vms[1])
+            try:
+                session_vm1.read_until_output_matches(
+                    [expected_msg], timeout=15)
+            except aexpect.ExpectError:
+                test.fail("Can't connect TLS client inside TLS server guest.")
+            vm2 = env.get_vm(vms[1])
+            session_vm1.close()
+            vm1.destroy()
+            vm2.destroy()
+    finally:
+        gnutls_pid = process.getoutput("pgrep -f gnutls", shell=True)
+        if gnutls_pid:
+            process.run("pkill -9 gnutls")
+        process.run(clean_cmd)

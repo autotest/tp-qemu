8d471b643a4edfcec1fdc2576b39404f51e3a286
Yihuang Yu <yihyu@redhat.com>
2021-01-05 13:28:48 +0800

    New case of controlling life cycle of guest via external server
    After launching a guest, create a subscriber and waiting for the server
    to enter the "APPROVE" message to continue the test.
    
    Signed-off-by: Yihuang Yu <yihyu@redhat.com>

---
+++ qemu/tests/nested_interactive_agent.py
@@ -0,0 +1,66 @@
+import logging
+
+from virttest import error_context
+from virttest.utils_test import VMStress
+
+from provider import message_queuing
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Control the life cycle of the guest through chat of remote MQ server
+
+    Step:
+     1) Launch a guest.
+     2) Create a message queuing subscriber.
+     3) Run stress tool in guest if necessary.
+     4) Waiting for the MQ server to enter the "APPROVE" message.
+     5) Reboot guest after received the message.
+     6) Destroy the guest finally.
+
+    :param test: Qemu test object.
+    :param params: Dictionary with the test parameters.
+    :param env: Dictionary with test environment.
+    """
+
+    run_stress = params.get_boolean('run_stress')
+    mq_publisher = params['mq_publisher']
+    mq_port = params.get('mq_port')
+    wait_response_timeout = params.get_numeric('wait_response_timeout', 600)
+
+    vm = env.get_vm(params['main_vm'])
+    vm.verify_alive()
+    vm.wait_for_login()
+    stress_tool = None
+
+    if run_stress:
+        stress_tool = VMStress(vm, "stress", params)
+        stress_tool.load_stress_tool()
+
+    mq_subscriber = message_queuing.MQSubscriber(mq_publisher, mq_port)
+
+    try:
+        error_context.context('Receive the "APPROVE" message from MQ publisher '
+                              'to continue the test.', logging.info)
+        try:
+            event = mq_subscriber.receive_event(wait_response_timeout)
+            if event == "NOTIFY":
+                logging.warning('Got "NOTIFY" message to finish test')
+                return
+            elif event != 'APPROVE':
+                test.fail('Got unwanted message from MQ publisher.')
+        except message_queuing.UnknownEventError as err:
+            logging.error(err)
+            test.error('The MQ publisher did not enter the "APPROVE" message '
+                       'within the expected time.')
+        logging.info('Already captured the "APPROVE" message.')
+
+        if not stress_tool:
+            vm.reboot()
+    finally:
+        if stress_tool:
+            stress_tool.clean()
+        vm.verify_kernel_crash()
+        vm.destroy()
+        mq_subscriber.close()

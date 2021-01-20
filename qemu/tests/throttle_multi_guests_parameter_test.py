91ba018b778556d81965ba5acb8524cb694ffb06
Zixi Chen <zixchen@redhat.com>
2021-01-20 10:22:49 +0800

    Throttle: Add multi-vms parameter test
    Signed-off-by: Zixi Chen <zixchen@redhat.com>

---
+++ qemu/tests/throttle_multi_guests_parameter_test.py
@@ -0,0 +1,93 @@
+import os
+import logging
+import shutil
+
+from virttest import error_context
+from virttest import qemu_storage
+from virttest import data_dir
+from virttest import env_process
+from virttest import utils_misc
+
+from provider.storage_benchmark import generate_instance
+from provider.throttle_utils import ThrottleGroupManager, ThrottleTester, \
+    ThrottleGroupsTester
+
+
+# This decorator makes the test function aware of context strings
+@error_context.context_aware
+def run(test, params, env):
+    """
+        Test throttle relevant properties feature.
+
+        1) Boot up guest with throttle groups.
+        There are two throttle groups and each have two disk
+        2) Build fio operation options and expected result
+         according to throttle properties.
+        3) Execute one disk or all disks testing on groups parallel.
+    """
+
+    def copy_base_vm_image():
+        """Copy the base vm image for VMs."""
+        src_img = qemu_storage.QemuImg(
+            params, data_dir.get_data_dir(), params['images'])
+        src_filename = src_img.image_filename
+        src_format = src_img.image_format
+        dst_dir = os.path.dirname(src_filename)
+        for vm_name in vms_list:
+            dst_filename = os.path.join(
+                dst_dir, '%s.%s' % (vm_name, src_format))
+            if not os.path.exists(dst_filename):
+                logging.info('Copying %s to %s.', src_filename, dst_filename)
+                shutil.copy(src_filename, dst_filename)
+
+    def wait_for_login_all_vms():
+        """Wait all VMs to login."""
+        return [vm.wait_for_login() for vm in vms]
+
+    @error_context.context_aware
+    def fio_on_vm(vm_t, session_t):
+        error_context.context("Deploy fio", logging.info)
+        fio = generate_instance(params, vm_t, 'fio')
+        logging.info("fio: %s" % fio)
+        tgm = ThrottleGroupManager(vm_t)
+        logging.info("tgm: %s" % tgm)
+        groups = params["throttle_groups"].split()
+        testers = []
+        for group in groups:
+            tgm.get_throttle_group_props(group)
+            images = params["throttle_group_member_%s" % group].split()
+            tester = ThrottleTester(test, params, vm_t, session_t,
+                                    group, images)
+            error_context.context("Build test stuff for %s:%s"
+                                  % (group, images), logging.info)
+            tester.build_default_option()
+            tester.build_images_fio_option()
+            tester.set_fio(fio)
+            testers.append(tester)
+        error_context.context("Start groups testing:%s" % groups, logging.info)
+        groups_tester = ThrottleGroupsTester(testers)
+        groups_tester.start()
+
+    def fio_on_vms():
+        """Run fio on all started vms at the same time."""
+        logging.info("Start to do fio on  multi-vms:")
+        fio_parallel_params = []
+        for vm, session in zip(vms, sessions):
+            fio_parallel_params.append((fio_on_vm, (vm, session)))
+        utils_misc.parallel(fio_parallel_params)
+        logging.info("Done fio on multi-vms.")
+
+    vms_list = params['vms'].split()
+    copy_base_vm_image()
+    vms_default = params['vms'].split()[0]
+    vms_post = params['vms'].split(vms_default)[1].strip()
+    params['vms'] = str(vms_post)
+    params['start_vm'] = 'yes'
+    env_process.process(test, params, env,
+                        env_process.preprocess_image,
+                        env_process.preprocess_vm)
+    vms = env.get_all_vms()
+    for vm_verify in vms:
+        vm_verify.verify_alive()
+    sessions = wait_for_login_all_vms()
+    fio_on_vms()

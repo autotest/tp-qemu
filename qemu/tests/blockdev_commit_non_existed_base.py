dd884d535a65b8ff29850835117ce5994dae9820
Leidong Wang <leidwang@redhat.com>
2021-02-02 18:10:12 +0800

    blockdev_commit_non_existed_base: Block commit test
    Block commit with base is an non-existed snapshot in snapshot
    chain. When do live commit, specify an non-existed snapshot
    in snapshot chain as base, qemu-kvm should prompt error.
    
    Signed-off-by: Leidong Wang <leidwang@redhat.com>

---
+++ qemu/tests/blockdev_commit_non_existed_base.py
@@ -0,0 +1,54 @@
+import logging
+
+from virttest.qemu_monitor import QMPCmdError
+
+from provider import backup_utils
+from provider.blockdev_commit_base import BlockDevCommitTest
+
+
+class BlockdevCommitNonExistedBase(BlockDevCommitTest):
+
+    def commit_snapshots(self):
+        device = self.params.get("device_tag")
+        device_params = self.params.object_params(device)
+        snapshot_tags = device_params["snapshot_tags"].split()
+        self.device_node = self.get_node_name(device)
+        options = ["base-node", "top-node", "speed"]
+        arguments = self.params.copy_from_keys(options)
+        arguments["base-node"] = self.params["none_existed_base"]
+        device = self.get_node_name(snapshot_tags[-1])
+        arguments["top-node"] = device
+        commit_cmd = backup_utils.block_commit_qmp_cmd
+        cmd, args = commit_cmd(device, **arguments)
+        try:
+            self.main_vm.monitor.cmd(cmd, args)
+        except QMPCmdError as e:
+            logging.info("Error message is %s" % e.data)
+            if self.params["qmp_error_msg"] not in str(e.data):
+                self.test.fail("Error message not as expected")
+        else:
+            self.test.fail("Block commit should fail with "
+                           "'Cannot find device= nor node_name=sn0'"
+                           ",but block commit succeeded unexpectedly")
+
+    def run_test(self):
+        self.pre_test()
+        try:
+            self.commit_snapshots()
+        finally:
+            self.post_test()
+
+
+def run(test, params, env):
+    """
+    Block commit with base is an non-existed snapshot in snapshot chain
+
+    1. boot guest with data disk
+    2. create 4 snapshots and save file in each snapshot
+    3. specify an non-existed snapshot sn0 in snapshot chain as base
+    4. do live commit
+    5. check QMPCmdError data
+    """
+
+    block_test = BlockdevCommitNonExistedBase(test, params, env)
+    block_test.run_test()

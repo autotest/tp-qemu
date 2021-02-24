f21e4623a3f7d44a3d7dc431a01217a0c942a01d
MiriamDeng <mdeng@redhat.com>
2021-02-24 14:26:32 +0800

    hpt:checking if P8 guest's negative test on power9 host pass or not
    Signed-off-by: MiriamDeng <mdeng@redhat.com>

---
+++ qemu/tests/hpt_huge_page_negative.py
@@ -0,0 +1,32 @@
+import logging
+import re
+
+from virttest import error_context
+from virttest import virt_vm
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    hpt guest's negative test:
+    1) Boot vm with incorrect hpt_huge_page value options
+    2) Check if can get the expected qemu output
+
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+
+    vm = env.get_vm(params["main_vm"])
+    error_msg = params.get('error_msg')
+    try:
+        vm.create(params=params)
+    except virt_vm.VMCreateError as e:
+        o = e.output
+    else:
+        test.fail("Test failed since vm shouldn't be launched")
+    error_context.context("Check the expected error message: %s"
+                          % error_msg, logging.info)
+    if not re.search(error_msg, o):
+        test.fail("Can not get expected error message: %s from %s"
+                  % (error_msg, o))

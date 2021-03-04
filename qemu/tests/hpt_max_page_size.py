c51cc71c7fe09045f8db23533e448984567e4748
MiriamDeng <mdeng@redhat.com>
2021-03-04 09:57:14 +0800

    huge_page: boot up p8 guest with 16M page size on power9
    Signed-off-by: MiriamDeng <mdeng@redhat.com>

--- qemu/tests/hpt_max_page_size.py
+++ qemu/tests/hpt_max_page_size.py
@@ -19,6 +19,8 @@ def run(test, params, env):
        when the cap-hpt-max-page-size is 64k.
     6) Check output parameter the Hugepagesize is 16384,
        when the cap-hpt-max-page-size is 16M.
+    7) Boot up a guest with cap-hpt-max-page-size = 16M on the host with
+    default huge page size
 
     :params test: QEMU test object.
     :params params: Dictionary with the test parameters.
@@ -28,7 +30,6 @@ def run(test, params, env):
         meminfo = session.cmd_output("grep %s /proc/meminfo" % key)
         actual_value = re.search(r'\d{4,}', meminfo)
         return actual_value.group(0) if actual_value else ""
-
     timeout = params.get_numeric("login_timeout", 240)
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
@@ -38,3 +39,17 @@ def run(test, params, env):
     if _check_meminfo("Hugepagesize") != params["expected_value"]:
         test.fail("The hugepage size doesn't match, "
                   "please check meminfo: %s " % _check_meminfo("Huge"))
+    # Please set 1G huge page as default huge page size on power9
+    if params.get("sub_type") == "hugepage_reset":
+        origin_nr = params.get("origin_nr")
+        error_context.context("Setup hugepage number to %s in guest" % origin_nr, logging.info)
+        set_hugepage_cmd = params.get("set_hugepage_cmd")
+        if session.cmd_status(set_hugepage_cmd):
+            test.fail("Failed to assign nr in the guest")
+        check_result_cmd = params.get("check_result_cmd")
+        output = session.cmd_output(check_result_cmd)
+        result_value = re.search(r'\d{1,}', output).group(0)
+        if result_value != origin_nr:
+            test.fail("Assigned nr %s doesn't match expected %s" % (result_value, origin_nr))
+    vm.verify_kernel_crash()
+    vm.destroy()

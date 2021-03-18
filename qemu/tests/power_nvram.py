94af64a12fda0d82d4c00433f7954936dd058e27
MiriamDeng <mdeng@redhat.com>
2021-03-18 17:20:00 +0800

    power_nvram: make sure qemu-kvm works well while raw strings are passed
    Signed-off-by: MiriamDeng <mdeng@redhat.com>

---
+++ qemu/tests/power_nvram.py
@@ -0,0 +1,38 @@
+import logging
+import re
+
+from virttest import error_context
+from virttest import virt_vm
+from virttest import qemu_monitor
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    nvram test on power:
+    1) Boot vm with different raw string for nvram
+    2) Qemu works as expected
+
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+
+    vm = env.get_vm(params["main_vm"])
+    error_msg = params.get('nvram_expected_result')
+    nvram_sub_type = params.get("nvram_sub_type")
+    if nvram_sub_type != "normal":
+        try:
+            vm.create(params=params)
+        except virt_vm.VMCreateError as e:
+            output = e.output
+            error_context.context("Check the expected error message: %s"
+                                  % error_msg, logging.info)
+            if not re.search(error_msg, output):
+                test.fail("Can not get expected error message: %s from %s"
+                          % (error_msg, output))
+        except qemu_monitor.MonitorConnectError:
+            pass
+    else:
+        vm.wait_for_login()
+        vm.verify_kernel_crash()

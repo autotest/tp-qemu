4870f5475d5cc425bab6b4afa7d0f44cb8be783a
Lei Yang <leiayang@redhat.com>
2021-04-28 09:27:35 +0800

    Solve the problem of broken pipe caused by lack of session
    1.Change netperf test function to netperf module
    
    Signed-off-by: Lei Yang <leiayang@redhat.com>

--- qemu/tests/queues_number_test.py
+++ qemu/tests/queues_number_test.py
@@ -1,13 +1,12 @@
 import logging
 import re
-import os
 
 from virttest import error_context
-from virttest import data_dir
 from virttest import utils_net
 from virttest import utils_test
 from virttest import utils_misc
-from virttest import utils_netperf
+
+from provider import netperf_test
 
 
 @error_context.context_aware
@@ -85,59 +84,12 @@ def run(test, params, env):
                 output.splitlines()[-3:])
             test.fail(err)
 
-    def netperf_test():
-        """
-        Netperf stress test for nic option.
-        """
-        try:
-            n_server.start()
-            # Run netperf with message size defined in range.
-            netperf_test_duration = params.get_numeric("netperf_test_duration")
-            test_protocols = params.get("test_protocols", "TCP_STREAM")
-            netperf_output_unit = params.get("netperf_output_unit")
-            test_option = params.get("test_option", "")
-            test_option += " -l %s" % netperf_test_duration
-            if netperf_output_unit in "GMKgmk":
-                test_option += " -f %s" % netperf_output_unit
-            t_option = "%s -t %s" % (test_option, test_protocols)
-            n_client.bg_start(utils_net.get_host_ip_address(params),
-                              t_option,
-                              params.get_numeric("netperf_para_sessions"),
-                              params.get("netperf_cmd_prefix", ""),
-                              package_sizes=params.get("netperf_sizes"))
-            if utils_misc.wait_for(n_client.is_netperf_running, 10, 0, 1,
-                                   "Wait netperf test start"):
-                logging.info("Netperf test start successfully.")
-            else:
-                test.error("Can not start netperf client.")
-            utils_misc.wait_for(
-                lambda: not n_client.is_netperf_running(),
-                netperf_test_duration, 0, 5,
-                "Wait netperf test finish %ss" % netperf_test_duration)
-        finally:
-            n_server.stop()
-
     login_timeout = params.get_numeric("login_timeout", 360)
     netperf_stress = params.get("run_bgstress")
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
     vm.wait_for_serial_login(timeout=login_timeout)
     guest_ip = vm.get_address()
-    n_client = utils_netperf.NetperfClient(
-        guest_ip, params.get("client_path"),
-        netperf_source=os.path.join(data_dir.get_deps_dir("netperf"),
-                                    params.get("netperf_client_link")),
-        client=params.get("shell_client"),
-        username=params.get("username"),
-        password=params.get("password"),
-        compile_option=params.get("compile_option", ""))
-    n_server = utils_netperf.NetperfServer(
-        utils_net.get_host_ip_address(params),
-        params.get("server_path", "/var/tmp"),
-        netperf_source=os.path.join(data_dir.get_deps_dir("netperf"),
-                                    params.get("netperf_server_link")),
-        password=params.get("hostpassword"),
-        compile_option=params.get("compile_option", ""))
     wait_time = params.get_numeric("wait_bg_time")
     ping_lost_ratio = params.get_numeric("background_ping_package_lost_ratio", 5)
     ping_time = params.get_numeric("background_ping_time")
@@ -146,7 +98,6 @@ def run(test, params, env):
     try:
         while bg_test:
             session_serial = vm.wait_for_serial_login(timeout=login_timeout)
-            n_client.session = session_serial
             error_context.context("Enable multi queues in guest.", logging.info)
             for nic in vm.virtnet:
                 ifname = utils_net.get_linux_ifname(session_serial, nic.mac)
@@ -154,7 +105,8 @@ def run(test, params, env):
                 change_queues_number(ifname, queues)
             error_context.context("Run test %s background" % netperf_stress,
                                   logging.info)
-            stress_thread = utils_misc.InterruptedThread(netperf_test)
+            stress_thread = utils_misc.InterruptedThread(
+                netperf_test.netperf_stress, (test, params, vm))
             stress_thread.start()
             utils_misc.wait_for(lambda: wait_time, 0, 5,
                                 "Wait %s start background" % netperf_stress)
@@ -207,7 +159,5 @@ def run(test, params, env):
             else:
                 bg_test = False
     finally:
-        n_server.cleanup(True)
-        n_client.cleanup(True)
         if session_serial:
             session_serial.close()

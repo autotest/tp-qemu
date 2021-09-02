17eb67be794e57eefd041fe47a41a2275687a280
Aihua Liang <aliang@redhat.com>
2021-09-02 15:09:50 +0800

    blockdev_commit_auto_readonly: support auto_read_only for commit node
    For blockdev, auto-read-only is off in default since qemu3.1. In this
    case we test commit with auto-read-only "on".
    
    Signed-off-by: Aihua Liang <aliang@redhat.com>

---
+++ qemu/tests/blockdev_commit_auto_readonly.py
@@ -0,0 +1,31 @@
+from provider import backup_utils
+from provider import job_utils
+from provider.blockdev_commit_base import BlockDevCommitTest
+
+
+class BlockdevCommitAutoReadonly(BlockDevCommitTest):
+
+    def commit_snapshots(self):
+        device = self.params.get("device_tag")
+        device_params = self.params.object_params(device)
+        snapshot_tags = device_params["snapshot_tags"].split()
+        device = self.get_node_name(snapshot_tags[-1])
+        commit_cmd = backup_utils.block_commit_qmp_cmd
+        cmd, args = commit_cmd(device)
+        self.main_vm.monitor.cmd(cmd, args)
+        job_id = args.get("job-id", device)
+        job_utils.wait_until_block_job_completed(self.main_vm, job_id)
+
+
+def run(test, params, env):
+    """
+    Block commit base Test
+
+    1). boot guest with system disk
+    2). create 2 snapshots and save file in each snapshot
+    3). commit from sn2 to base
+    4). verify files's md5
+    """
+
+    block_test = BlockdevCommitAutoReadonly(test, params, env)
+    block_test.run_test()

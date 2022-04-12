e8b644e9c3cdbca163eabd112a7152d0924229f0
zhenyzha <zhenyzha@redhat.com>
2022-04-12 10:20:24 +0800

    block_with_share_rw:add environment checks
    Some env the image in indirect_image_blacklist
    does not exist in host. So we need to check the host env
    first and update the blacklist.
    
    Signed-off-by: zhenyzha <zhenyzha@redhat.com>

--- qemu/tests/block_with_share_rw.py
+++ qemu/tests/block_with_share_rw.py
@@ -1,3 +1,7 @@
+import re
+
+from avocado.utils import process
+
 from virttest import error_context
 from virttest import env_process
 from virttest import virt_vm
@@ -22,6 +26,29 @@ def run(test, params, env):
     :param env: Dictionary with test environment.
     """
     msgs = ['"write" lock', 'Is another process using the image']
+    modprobe_cmd = params.get('modprobe_cmd')
+    disk_check_cmd = params.get('disk_check_cmd')
+    indirect_image_blacklist = params.get('indirect_image_blacklist').split()
+
+    # In some environments, The image in indirect_image_blacklist
+    # does not exist in host. So we need to check the host env
+    # first and update the blacklist.
+    if disk_check_cmd:
+        image_stg_blacklist = params.get('image_stg_blacklist').split()
+        matching_images = process.run(disk_check_cmd, ignore_status=True,
+                                      shell=True).stdout_text
+        for disk in image_stg_blacklist:
+            if not re.search(disk, matching_images):
+                indirect_image_blacklist.remove(disk)
+        params["indirect_image_blacklist"] = " ".join(indirect_image_blacklist)
+
+        process.run(modprobe_cmd, ignore_status=True, shell=True)
+        params["image_raw_device_stg"] = "yes"
+        params["indirect_image_select_stg"] = "-1"
+        params["start_vm"] = "yes"
+        env_process.preprocess_vm(test, params, env,
+                                  params["main_vm"])
+
     vm1 = env.get_vm(params["main_vm"])
     vm1.verify_alive()
     vm1.wait_for_login(timeout=360)

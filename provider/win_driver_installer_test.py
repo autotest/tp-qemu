953d097ca5a90b62bf6b7a63959267a547ea170b
Houqi (Nick) Zuo <hzuo@redhat.com>
2022-08-17 14:00:05 +0800

    win_virtio_driver_install_by_installer.py: add new test case
    Signed-off-by: Houqi (Nick) Zuo hzuo@redhat.com

--- provider/win_driver_installer_test.py
+++ provider/win_driver_installer_test.py
@@ -1,8 +1,13 @@
 import logging
 import time
+import os
 
+from avocado.utils import process
+
+from virttest import data_dir
 from virttest import error_context
 from virttest import utils_misc
+from virttest.utils_windows import virtio_win
 
 from provider import win_driver_utils
 
@@ -197,3 +202,223 @@ def check_gagent_version(session, test, gagent_pkg_info_cmd,
     if actual_gagent_version != expected_gagent_version:
         test.fail("gagent version is not right, expected is %s but got %s"
                   % (expected_gagent_version, actual_gagent_version))
+
+
+@error_context.context_aware
+def get_viofs_exe(test, params, session):
+    """
+    Get viofs.exe from virtio win iso,such as E:\viofs\2k19\amd64
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    error_context.context("Get virtiofs exe full path.", test.log.info)
+    media_type = params["virtio_win_media_type"]
+    try:
+        get_drive_letter = getattr(virtio_win, "drive_letter_%s" % media_type)
+        get_product_dirname = getattr(virtio_win,
+                                      "product_dirname_%s" % media_type)
+        get_arch_dirname = getattr(virtio_win, "arch_dirname_%s" % media_type)
+    except AttributeError:
+        test.error("Not supported virtio win media type '%s'", media_type)
+    viowin_ltr = get_drive_letter(session)
+    if not viowin_ltr:
+        test.error("Could not find virtio-win drive in guest")
+    guest_name = get_product_dirname(session)
+    if not guest_name:
+        test.error("Could not get product dirname of the vm")
+    guest_arch = get_arch_dirname(session)
+    if not guest_arch:
+        test.error("Could not get architecture dirname of the vm")
+
+    exe_middle_path = ("{name}\\{arch}" if media_type == "iso"
+                       else "{arch}\\{name}").format(name=guest_name,
+                                                     arch=guest_arch)
+    exe_file_name = "virtiofs.exe"
+    exe_find_cmd = 'dir /b /s %s\\%s | findstr "\\%s\\\\"'
+    exe_find_cmd %= (viowin_ltr, exe_file_name, exe_middle_path)
+    exe_path = session.cmd(exe_find_cmd).strip()
+    test.log.info("Found exe file '%s'", exe_path)
+    return exe_path
+
+
+def viofs_svc_create(test, params, session):
+    """
+    Only for windows guest, to create a virtiofs service
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    viofs_sc_create_cmd = params["viofs_sc_create_cmd"]
+    test.log.info("Register virtiofs service in Windows guest.")
+    exe_path = get_viofs_exe(test, params, session)
+    sc_create_s, sc_create_o = session.cmd_status_output(viofs_sc_create_cmd
+                                                         % exe_path)
+    if sc_create_s != 0:
+        test.fail(
+            "Failed to register virtiofs service, output is %s" % sc_create_o)
+
+
+@error_context.context_aware
+def viofs_svc_delete(test, params, session):
+    """
+    delete the virtiofs service on Windows guest
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    viofs_sc_delete_cmd = params["viofs_sc_delete_cmd"]
+    error_context.context("Deleting the viofs service...", test.log.info)
+    status = session.cmd_status(viofs_sc_delete_cmd)
+    if status == 0:
+        error_context.context("Done to delete the viofs service...",
+                              test.log.info)
+    else:
+        error_context.context("Failed to delete the viofs service...",
+                              test.fail)
+
+
+@error_context.context_aware
+def viofs_svc_start(test, params, seesion):
+    """
+    start the virtiofs service on Windows guest
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    viofs_sc_start_cmd = params["viofs_sc_start_cmd"]
+    error_context.context("Trying to start the viofs service...", test.log.info)
+    status = seesion.cmd_status(viofs_sc_start_cmd)
+    if status == 0:
+        error_context.context("Done to start the viofs service.", test.log.info)
+    else:
+        error_context.context("Failed to start the viofs service...",
+                              test.fail)
+
+
+@error_context.context_aware
+def viofs_svc_query(test, params, session):
+    """
+    query the virtiofs service on Windows guest
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    viofs_sc_query_cmd = params["viofs_sc_query_cmd"]
+    error_context.context("Query the status of viofs service...", test.log.info)
+    output = session.cmd_output(viofs_sc_query_cmd)
+    return output
+
+
+@error_context.context_aware
+def viofs_svc_run(test, params, session):
+    """
+    run the virtiofs service on Windows guest
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    output = viofs_svc_query(test, params, session)
+    if "not exist as an installed service" not in output:
+        error_context.context("Clean virtiofs service registered")
+        viofs_svc_delete(test, params, session)
+    test.log.info("Register virtiofs service")
+    viofs_svc_create(test, params, session)
+
+    test.log.info("Check if virtiofs service is started.")
+    output = viofs_svc_query(test, params, session)
+    if "RUNNING" not in output:
+        viofs_svc_start(test, params, session)
+    else:
+        test.log.info("Virtiofs service is running.")
+
+
+def install_winfsp(test, params, session):
+    """
+    install the winfsp on Windows guest
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    cmd_timeout = params.get_numeric("cmd_timeout", 120)
+    install_path = params["install_path"]
+    check_installed_cmd = params["check_installed_cmd"] % install_path
+    # install winfsp tool
+    test.log.info("Install winfsp for windows guest.")
+    installed = session.cmd_status(check_installed_cmd) == 0
+    if installed:
+        test.log.info("Winfsp tool is already installed.")
+    else:
+        install_cmd = utils_misc.set_winutils_letter(session,
+                                                     params["install_cmd"])
+        session.cmd(install_cmd, cmd_timeout)
+        if not utils_misc.wait_for(lambda: not session.cmd_status(
+                check_installed_cmd), 60):
+            test.error("Winfsp tool is not installed.")
+
+
+def get_fs_dest_from_vm(test, params, session):
+    """
+    get the fs dest from vm( Windows guest only )
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    :return guest_file: the full path of file
+    :return fs_dest: the volume letter with colon
+    :return volume_letter: the volume letter
+    """
+    test_file = params.get('test_file', "test_file")
+    virtio_fs_disk_label = params.get("fs_target")
+    test.log.info("Get Volume letter of virtio fs target, the disk lable is "
+                  "%s." % virtio_fs_disk_label)
+    vol_con = "VolumeName='%s'" % virtio_fs_disk_label
+    volume_letter = utils_misc.wait_for(
+        lambda: utils_misc.get_win_disk_vol(session, condition=vol_con),
+        timeout=120)
+    if volume_letter is None:
+        test.fail("Could not get virtio-fs mounted volume letter.")
+    fs_dest = "%s:" % volume_letter
+
+    guest_file = os.path.join(fs_dest, test_file)
+    test.log.info("The guest file in shared dir is %s", guest_file)
+    return guest_file, fs_dest, volume_letter
+
+
+def basic_io_test(test, params, session):
+    """
+    basic io test in Windows guest
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param session: the seesion of guest
+    """
+    test_file = params.get('test_file', "test_file")
+    cmd_dd = params.get("cmd_dd")
+    io_timeout = params.get("timeout", 120)
+    cmd_md5 = params.get("cmd_md5")
+    fs_source = params.get("fs_source_dir")
+    base_dir = params.get("fs_source_base_dir",
+                          data_dir.get_data_dir())
+    if not os.path.isabs(fs_source):
+        fs_source = os.path.join(base_dir, fs_source)
+
+    host_data = os.path.join(fs_source, test_file)
+
+    guest_file, fs_dest, volume_letter = get_fs_dest_from_vm(test,
+                                                             params,
+                                                             session)
+
+    test.log.info("Creating file under %s inside guest." % fs_dest)
+    session.cmd(cmd_dd % guest_file, io_timeout)
+
+    guest_file_win = guest_file.replace("/", "\\")
+    cmd_md5_vm = cmd_md5 % (volume_letter, guest_file_win)
+    md5_guest = session.cmd_output(cmd_md5_vm, io_timeout).strip().split()[0]
+
+    test.log.info("md5 of the guest file: " + md5_guest)
+    md5_host = process.run("md5sum %s" % host_data,
+                           io_timeout).stdout_text.strip().split()[0]
+    test.log.info("md5 of the host file: " + md5_host)
+    if md5_guest != md5_host:
+        test.fail('The md5 value of host is not same to guest.')
+    else:
+        test.log.info("The md5 value of host is as same as md5 value of guest.")
--- qemu/tests/win_virtio_driver_install_by_installer.py
+++ qemu/tests/win_virtio_driver_install_by_installer.py
@@ -7,6 +7,7 @@ from provider.win_driver_installer_test import (install_gagent,
                                                 check_gagent_version,
                                                 driver_check,
                                                 install_test_with_screen_on_desktop)
+from provider import win_driver_installer_test
 
 
 @error_context.context_aware
@@ -36,23 +37,51 @@ def run(test, params, env):
     gagent_pkg_info_cmd = params["gagent_pkg_info_cmd"]
     gagent_install_cmd = params["gagent_install_cmd"]
     gagent_uninstall_cmd = params["gagent_uninstall_cmd"]
+    register_virtio_fs_service = params.get("register_virtio_fs_service", "")
+    basic_io_test = params.get("basic_io_test", "")
 
     vm = env.get_vm(params["main_vm"])
     session = vm.wait_for_login()
+
+    if register_virtio_fs_service:
+        win_driver_installer_test.install_winfsp(test, params, session)
+        win_driver_installer_test.viofs_svc_run(test, params, session)
+
+    if basic_io_test:
+        win_driver_installer_test.basic_io_test(test, params, session)
 
     expected_gagent_version = install_gagent(session, test,
                                              qemu_ga_pkg,
                                              gagent_install_cmd,
                                              gagent_pkg_info_cmd)
+
+    if basic_io_test:
+        win_driver_installer_test.basic_io_test(test, params, session)
+
     uninstall_gagent(session, test, gagent_uninstall_cmd)
+
+    if basic_io_test:
+        win_driver_installer_test.basic_io_test(test, params, session)
+
     win_uninstall_all_drivers(session, test, params)
+
+    if basic_io_test:
+        win_driver_installer_test.basic_io_test(test, params, session)
+
     session = vm.reboot(session)
     install_test_with_screen_on_desktop(vm, session, test, run_install_cmd,
                                         installer_pkg_check_cmd,
                                         copy_files_params=params)
+
+    if basic_io_test:
+        win_driver_installer_test.basic_io_test(test, params, session)
+
     win_installer_test(session, test, params)
     check_gagent_version(session, test, gagent_pkg_info_cmd,
                          expected_gagent_version)
     driver_check(session, test, params)
+
+    if basic_io_test:
+        win_driver_installer_test.basic_io_test(test, params, session)
 
     session.close()

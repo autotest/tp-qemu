73b18ff7ef0705979bb4dc187e9c124af55c1742
qingwangrh <qinwang@redhat.com>
2022-09-06 05:25:31 -0400

    Add QSD object option test
    Signed-off-by: qingwangrh <qinwang@redhat.com>

---
+++ qemu/tests/qsd_object_check.py
@@ -0,0 +1,36 @@
+"""QSD throttle object test"""
+
+from provider.qsd import QsdDaemonDev
+from virttest import error_context
+
+
+# This decorator makes the test function aware of context strings
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Test QSD throttle object .
+    Steps:
+        1) Run QSD with throttle node.
+        2) Check throttle object attribute.
+    """
+    logger = test.log
+    qsd = None
+    try:
+        qsd = QsdDaemonDev("qsd1", params)
+        qsd.start_daemon()
+
+        qsd.monitor.cmd("query-block-exports")
+        qsd.monitor.cmd("query-named-block-nodes")
+        for tg in params["check_groups"].split():
+            logger.info("Check throttle %s", tg)
+            out = qsd.monitor.qom_get(tg, "limits")
+            test.assertEqual(out[params["key_%s" % tg]],
+                             int(params["value_%s" % tg]),
+                             "Unexpected throttle values :%s" % tg)
+        qsd.stop_daemon()
+        qsd = None
+    finally:
+        if qsd:
+            qsd.stop_daemon()
+
+    logger.info("Test Over")

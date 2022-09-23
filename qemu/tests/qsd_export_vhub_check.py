044b21fb42c5bc1218a61748308e1cdfa699968b
qingwangrh <qinwang@redhat.com>
2022-09-23 04:57:57 -0400

    Add QSD export vhost-user-blk option test
    Signed-off-by: qingwangrh <qinwang@redhat.com>

---
+++ qemu/tests/qsd_export_vhub_check.py
@@ -0,0 +1,34 @@
+"""QSD export vhost-user-blk option test"""
+
+from provider.qsd import QsdDaemonDev
+from virttest import error_context
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Test pidfile option .
+    Steps:
+        1) Run QSD with different export vhost-user-blk option.
+        2) Check the export number.
+    """
+
+    logger = test.log
+    qsd = None
+    try:
+        qsd_name = params["qsd_namespaces"]
+        qsd = QsdDaemonDev(qsd_name, params)
+        qsd.start_daemon()
+
+        out = qsd.monitor.cmd("query-block-exports")
+        qsd_params = params.object_params(qsd_name)
+        qsd_images = qsd_params.get("qsd_images").split()
+        if len(qsd_images) != len(out):
+            test.fail("Find mismatch number of export")
+        qsd.stop_daemon()
+        qsd = None
+    finally:
+        if qsd:
+            qsd.stop_daemon()
+
+    logger.info("Test Over")

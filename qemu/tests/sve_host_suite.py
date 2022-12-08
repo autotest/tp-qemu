12400918525700963f9939ea0a49be39b270c0bc
Yihuang Yu <yihyu@redhat.com>
2022-12-08 15:06:25 +0800

    sve_suite: Use kernel src.rpm for get_reg_list and sve_stress test
    Use the upstream git repo for testing is not stable, use the src.rpm
    for sve testing, the version is as same as the system.
    
    Signed-off-by: Yihuang Yu <yihyu@redhat.com>

--- qemu/tests/sve_host_suite.py
+++ qemu/tests/sve_host_suite.py
@@ -13,28 +13,40 @@ from provider import cpu_utils
 @error_context.context_aware
 def run(test, params, env):
     def compile_kernel_selftests():
-        git_cmd = 'git clone --depth=1 {} {} 2>/dev/null'.format(git_repo,
-                                                                 dst_dir)
+        error_context.context(
+            'Download the kernel src.rpm package and uncompress it',
+            test.log.info)
+        process.run(get_suite_cmd, shell=True)
         if os.path.exists(dst_dir):
             shutil.rmtree(dst_dir)
-        process.run(git_cmd, timeout=360, shell=True)
+        os.mkdir(dst_dir)
+        process.run(uncompress_cmd.format(tmp_dir, linux_name), shell=True)
+
+        error_context.context('Compile kernel selftests', test.log.info)
         s, o = process.getstatusoutput(compile_cmd, timeout=180)
         if s:
             test.log.error('Compile output: %s', o)
             test.error('Failed to compile the test suite.')
 
-    dst_dir = params['dst_dir']
-    git_repo = params['git_repo']
     compile_cmd = params['compile_cmd']
+    dst_dir = params['dst_dir']
     execute_suite_cmd = params['execute_suite_cmd']
+    get_suite_cmd = params['get_suite_cmd']
     required_pkgs = params.objects('required_pkgs')
     suite_timeout = params.get_numeric('suite_timeout')
+    uncompress_cmd = params['uncompress_cmd']
+    tmp_dir = test.tmpdir
 
     if not utils_package.package_install(required_pkgs):
         test.error("Failed to install required packages in host")
     error_context.base_context('Check if the CPU of host supports SVE',
                                test.log.info)
     cpu_utils.check_cpu_flags(params, 'sve', test)
+
+    kernel_version = os.uname()[2].rsplit('.', 1)[0]
+    srpm = f"kernel-{kernel_version}.src.rpm"
+    linux_name = f"linux-{kernel_version}"
+    get_suite_cmd = get_suite_cmd.format(tmp_dir, srpm)
 
     try:
         compile_kernel_selftests()

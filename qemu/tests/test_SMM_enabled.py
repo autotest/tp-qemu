8fc442a35a5565336b13b10d2ca8c9a6ce9df016
nanliu <nanliu@redhat.com>
2023-03-09 17:56:20 +0800

    test_SMM_enabled:no error occurs with SMM enabled
    Add one case using developer's script to cover one customer's
    bug since our current cases hardly to reproduce it.
    
    Signed-off-by: nanliu <nanliu@redhat.com>

---
+++ qemu/tests/test_SMM_enabled.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+from virttest import data_dir
+from virttest import error_context
+from virttest import env_process
+
+
+@error_context.context_aware
+def run(test, params, env):
+    """
+    Test SMM enabled by developer's script
+    1) Boot qemu with test binary smm_int_window.flat
+    2) get test results, should pass without error
+
+    :param test: QEMU test object.
+    :param params: Dictionary with test parameters.
+    :param env: Dictionary with the test environment.
+    """
+    source_file = params["source_file"]
+    src_test_binary = os.path.join(data_dir.get_deps_dir(), source_file)
+    error_msg = params["error_msg"]
+    params["kernel"] = src_test_binary
+
+    params["start_vm"] = "yes"
+    vm_name = params['main_vm']
+    env_process.preprocess_vm(test, params, env, vm_name)
+    vm = env.get_vm(vm_name)
+    time.sleep(10)
+    output = vm.process.get_output()
+    if error_msg in output:
+        test.fail("Test failed because of qemu core dump!")

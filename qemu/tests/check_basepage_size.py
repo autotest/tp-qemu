dbf56047827294e168cfd55831300e0917cff931
zhenyzha <zhenyzha@redhat.com>
2023-04-23 15:05:17 +0800

    check_basepage_size:Add mapping hugepage check
    1. Update basic page size to dynamic checking method
    2. Add mapping hugepage check
    
    Signed-off-by: zhenyzha <zhenyzha@redhat.com>

--- qemu/tests/check_basepage_size.py
+++ qemu/tests/check_basepage_size.py
@@ -1,7 +1,11 @@
+import os
+
 from avocado.utils import process
 
 from virttest import env_process
 from virttest import error_context
+
+from virttest.utils_numeric import normalize_data_size
 
 
 @error_context.context_aware
@@ -10,19 +14,30 @@ def run(test, params, env):
     Check the basic page size.
     Steps:
     1) Check system configuration basic page size on host.
-    2) Boot a guest on the host.
-    3) Check system configuration basic page size on guest.
+    2) Check the basic page size mapping to the hugepage size on host.
+    3) Boot a guest on the host.
+    4) Check system configuration basic page size on guest.
+    5) Check the basic page size mapping to the hugepage size on guest.
 
     :params test: QEMU test object.
     :params params: Dictionary with the test parameters.
     :params env: Dictionary with test environment.
     """
-    get_page_size = params.get('get_page_size')
+    get_basic_page = params.get('get_basic_page')
+    local_pglist = params.get('local_pglist')
+    basic_page_list = params.objects('basic_page_list')
 
     test.log.info("Check system configuration basic page size on host.")
-    host_page_size = process.system_output(get_page_size).decode()
-    if host_page_size != params.get("page_size_host"):
-        test.fail("Host page size is %s not as expected." % host_page_size)
+    host_basic_page = process.system_output(get_basic_page).decode()
+    if host_basic_page not in basic_page_list:
+        test.fail("Host basic page size is %s not as expected." % host_basic_page)
+
+    test.log.info("Check the basic page size mapping to the hugepage size on host.")
+    host_basic_page = normalize_data_size("%sB" % host_basic_page, "K")
+    hugepage_list = params.objects("mapping_pgsize_%sk" % host_basic_page)
+    host_local_pglist = os.listdir(local_pglist)
+    if sorted(host_local_pglist) != sorted(hugepage_list):
+        test.fail("Host huge page size is %s not as expected." % host_local_pglist)
 
     params["start_vm"] = "yes"
     env_process.preprocess_vm(test, params, env, params["main_vm"])
@@ -30,6 +45,13 @@ def run(test, params, env):
     session = vm.wait_for_login()
 
     test.log.info("Check system configuration basic page size on guest.")
-    guest_page_size = session.cmd(get_page_size).strip()
-    if guest_page_size != params.get("page_size_guest"):
-        test.fail("Guest page size is %s not as expected." % guest_page_size)
+    guest_basic_page = session.cmd(get_basic_page).strip()
+    if guest_basic_page not in basic_page_list:
+        test.fail("Guest page size is %s not as expected." % guest_basic_page)
+
+    test.log.info("Check the basic page size mapping to the hugepage size on guest.")
+    guest_basic_page = normalize_data_size("%sB" % guest_basic_page, "K")
+    hugepage_list = params.objects("mapping_pgsize_%sk" % guest_basic_page)
+    guest_local_pglist = session.cmd("ls %s" % local_pglist).strip().split()
+    if sorted(guest_local_pglist) != sorted(hugepage_list):
+        test.fail("Guest huge page size is %s not as expected." % guest_local_pglist)

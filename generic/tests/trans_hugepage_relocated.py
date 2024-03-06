a183ddcc4e478186d34d2911278f38e9f94c4f30
mcasquer <mcasquer@redhat.com>
2024-03-06 11:03:34 +0100

    trans_hugepage.relocated: fix existing issues
    Fixes existing issues with the params and variables casting.
    Removes the redhat prefix from cfg values. For memory
    fragmentation, uses the thp_fragment tool, now invoked
    from an external provider.
    
    Signed-off-by: mcasquer <mcasquer@redhat.com>

--- generic/tests/trans_hugepage_relocated.py
+++ generic/tests/trans_hugepage_relocated.py
@@ -1,10 +1,13 @@
+import logging
 import time
-import os
 import re
 
 from avocado.utils import process
+
 from virttest import utils_test
 from virttest.staging import utils_memory
+
+from provider import thp_fragment_tool
 
 
 def run(test, params, env):
@@ -21,23 +24,25 @@ def run(test, params, env):
     :param params: Dictionary with test parameters.
     :param env: Dictionary with the test environment.
     """
+
     def nr_hugepage_check(sleep_time, wait_time):
         time_last = 0
         while True:
             value = int(utils_memory.read_from_meminfo("AnonHugePages"))
+            logging.debug("The AnonHugePages value: %d", value)
             nr_hugepages.append(value)
             time_stamp = time.time()
             if time_last != 0:
                 if nr_hugepages[-2] != nr_hugepages[-1]:
                     time_last = time_stamp
                 elif time_stamp - time_last > wait_time:
-                    test.log.info("Huge page size stop changed")
+                    logging.info("Huge page size stop changed")
                     break
             else:
                 time_last = time_stamp
             time.sleep(sleep_time)
 
-    test.log.info("Relocated test start")
+    logging.info("Relocated test start")
     login_timeout = float(params.get("login_timeout", 360))
     vm = env.get_vm(params["main_vm"])
     vm.verify_alive()
@@ -45,11 +50,11 @@ def run(test, params, env):
 
     free_memory = utils_memory.read_from_meminfo("MemFree")
     hugepage_size = utils_memory.read_from_meminfo("Hugepagesize")
-    mem = params.get("mem")
-    vmsm = int(mem) + 128
+    mem = params.get_numeric("mem")
+    vmsm = mem + 128
     hugetlbfs_path = params.get("hugetlbfs_path", "/proc/sys/vm/nr_hugepages")
     if vmsm < int(free_memory) / 1024:
-        nr_hugetlbfs = vmsm * 1024 / int(hugepage_size)
+        nr_hugetlbfs = int(vmsm * 1024 / hugepage_size)
     else:
         nr_hugetlbfs = None
     # Get dd speed in host
@@ -63,47 +68,30 @@ def run(test, params, env):
     s_time = int(re.findall(r"scan_sleep_millisecs:(\d+)", thp_cfg)[0]) / 1000
     w_time = int(re.findall(r"alloc_sleep_millisecs:(\d+)", thp_cfg)[0]) / 1000
 
-    try:
-        test.log.info("Turn off swap in guest")
-        s, o = session.cmd_status_output("swapoff -a")
-        if s != 0:
-            test.log.warning("Didn't turn off swap in guest")
-        s, o = session.cmd_status_output("cat /proc/meminfo")
-        mem_free_filter = r"MemFree:\s+(.\d+)\s+(\w+)"
-        guest_mem_free, guest_unit = re.findall(mem_free_filter, o)[0]
-        if re.findall("[kK]", guest_unit):
-            guest_mem_free = str(int(guest_mem_free) / 1024)
-        elif re.findall("[gG]", guest_unit):
-            guest_mem_free = str(int(guest_mem_free) * 1024)
-        elif re.findall("[mM]", guest_unit):
-            pass
-        else:
-            guest_mem_free = str(int(guest_mem_free) / 1024 / 1024)
+    logging.info("Turn off swap in guest")
+    s, o = session.cmd_status_output("swapoff -a")
+    if s != 0:
+        logging.warning("Didn't turn off swap in guest")
+    s, o = session.cmd_status_output("cat /proc/meminfo")
+    mem_free_filter = r"MemFree:\s+(.\d+)\s+(\w+)"
+    guest_mem_free, guest_unit = re.findall(mem_free_filter, o)[0]
+    if re.findall("[kK]", guest_unit):
+        guest_mem_free = int(guest_mem_free) / 1024
+    elif re.findall("[gG]", guest_unit):
+        guest_mem_free = int(guest_mem_free) * 1024
+    elif re.findall("[mM]", guest_unit):
+        pass
+    else:
+        guest_mem_free = int(guest_mem_free) / 1024 / 1024
 
-        file_size = min(1024, int(guest_mem_free) / 2)
-        cmd = "mount -t tmpfs -o size=%sM none /mnt" % file_size
-        s, o = session.cmd_status_output(cmd)
-        if nr_hugetlbfs:
-            hugepage_cfg = open(hugetlbfs_path, "w")
+    file_size = min(1024, int(guest_mem_free / 2))
+    cmd = "mount -t tmpfs -o size=%sM none /mnt" % file_size
+    s, o = session.cmd_status_output(cmd)
+    if nr_hugetlbfs:
+        with open(hugetlbfs_path, "w") as hugepage_cfg:
             hugepage_cfg.write(str(nr_hugetlbfs))
-            hugepage_cfg.close()
 
-        if not os.path.isdir('/space'):
-            os.makedirs('/space')
-        if os.system("mount -t tmpfs -o size=%sM none /space" % vmsm):
-            test.error("Can not mount tmpfs")
-
-        # Try to make some fragment in memory
-        # The total size of fragments is vmsm
-        count = vmsm * 1024 / 4
-        cmd = "for i in `seq %s`; do dd if=/dev/urandom of=/space/$i" % count
-        cmd += " bs=4K count=1 & done"
-        test.log.info("Start to make fragment in host")
-        s = process.system(cmd, verbose=False, shell=True)
-        if s != 0:
-            test.error("Can not dd in host")
-    finally:
-        process.run("umount /space", verbose=False, shell=True)
+    thp_fragment_tool.execute_tool(test)
 
     bg = utils_test.BackgroundTest(nr_hugepage_check, (s_time, w_time))
     bg.start()
@@ -115,8 +103,7 @@ def run(test, params, env):
 
     if bg:
         bg.join()
-    mem_increase_step = int(re.findall(r"pages_to_scan:(\d+)",
-                                       thp_cfg)[0]) / 512
+    mem_increase_step = int(re.findall(r"pages_to_scan:(\d+)", thp_cfg)[0]) / 512
     mem_increase = 0
     w_step = w_time / s_time + 1
     count = 0
@@ -131,9 +118,12 @@ def run(test, params, env):
             mem_increase += last_value - current
             count = 0
         if count > w_step:
-            test.log.warning("Memory didn't increase in %s s", (count * s_time))
+            logging.warning("Memory didn't increase in %s s", (count * s_time))
+    logging.debug("The total mem_increase: %d", mem_increase)
     if mem_increase < file_size * 0.5:
-        test.error("Hugepages allocated can not reach a half: %s/%s"
-                   % (mem_increase, file_size))
+        test.error(
+            "Hugepages allocated can not reach a half: %s/%s"
+            % (mem_increase, file_size)
+        )
     session.close()
-    test.log.info("Relocated test succeed")
+    logging.info("Relocated test succeed")

0b6cf0889ea272e9a3aeed58a94a4edc51458d08
Zhenchao Liu <zhencliu@redhat.com>
2024-11-14 14:38:51 +0800

    image_multihost: Testing image on multihost
    Signed-off-by: Zhenchao Liu <zhencliu@redhat.com>

---
+++ qemu/tests/image_multihost.py
@@ -0,0 +1,32 @@
+from virttest.vt_imgr import imgr
+
+
+def run(test, params, env):
+    images = dict()
+    for image_tag in params.objects("images"):
+        images[image_tag] = imgr.query_logical_image(image_tag)
+        imgr.update_logical_image(images[image_tag], {"backup": {}})
+        test.log.info(
+            "after backup: %s: %s",
+            image_tag,
+            imgr.get_logical_image_info(images[image_tag]),
+        )
+        imgr.update_logical_image(images[image_tag], {"restore": {}})
+        test.log.info(
+            "after restore: %s: %s",
+            image_tag,
+            imgr.get_logical_image_info(images[image_tag]),
+        )
+
+    for image_id in images.values():
+        test.log.info("source: %s: %s", image_id, imgr.get_logical_image_info(image_id))
+        cloned_image_id = imgr.clone_logical_image(image_id)
+        test.log.info(
+            "cloned: %s: %s",
+            cloned_image_id,
+            imgr.get_logical_image_info(cloned_image_id),
+        )
+        imgr.update_logical_image(cloned_image_id, {"destroy": {}})
+        imgr.destroy_logical_image_object(cloned_image_id)
+        imgr.update_logical_image(image_id, {"destroy": {}})
+        imgr.destroy_logical_image_object(image_id)

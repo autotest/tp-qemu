cc6d71a30e31e2e1dd133da7a5c31710fe8c9bd0
Leidong Wang <leidwang@redhat.com>
2024-12-17 15:40:59 +0800

    Replace WMIC with powershell cmdlet
    From Win11-24H2, Windows remove WMIC, so replace it
    with some powershell cmdlet
    
    Signed-off-by: Leidong Wang <leidwang@redhat.com>

--- qemu/tests/windows_info.py
+++ qemu/tests/windows_info.py
@@ -25,23 +25,40 @@ def run(test, params, env):
     error_context.context("Get OS version and name.", test.log.info)
     output = session.cmd("ver")
     test.log.info("Windows version: %s", output.strip())
-    output = session.cmd("wmic os get Name")
-    output = output.strip().split()[-1]
+    get_os_name_cmd = (
+        'powershell -command "Get-CimInstance -ClassName Win32_OperatingSystem'
+        ' | Select-Object -ExpandProperty Caption"'
+    )
+    output = session.cmd(get_os_name_cmd)
+    output = output.strip()
     test.log.info("Windows name: %s", output)
 
     error_context.context("Get driver version information in guest.", test.log.info)
-    system_drivers = session.cmd("wmic sysdriver get DisplayName,PathName")
+    get_driver_info_cmd = (
+        'powershell -command "Get-CimInstance -ClassName Win32_SystemDriver'
+        ' | Select-Object DisplayName, PathName"'
+    )
+    system_drivers = session.cmd(get_driver_info_cmd)
     test.log.debug("Drivers exist in the system:\n %s", system_drivers)
     for i in system_drivers.splitlines():
         if re.findall(drivers_pattern, i, re.I):
             driver_info = i.strip().split()
             driver_name = " ".join(driver_info[:-1])
-            path = driver_info[-1]
+            get_driver_path_cmd = (
+                'powershell -command "Get-CimInstance -ClassName Win32_SystemDriver'
+                " | Where-Object {$_.DisplayName -eq '%s'}"
+                " | Select-Object PathName"
+                ' | Format-List"'
+            ) % driver_name
+            driver_path = session.cmd(get_driver_path_cmd)
+            path = driver_path.strip().split(" : ")[-1]
             path = re.sub(r"\\", "\\\\\\\\", path)
-            driver_ver_cmd = "wmic datafile where name="
-            driver_ver_cmd += "'%s' get version" % path
+            driver_ver_cmd = (
+                'powershell -command "(Get-Item -Path "%s").VersionInfo.FileVersion"'
+                % path
+            )
             output = session.cmd(driver_ver_cmd)
             msg = "Driver %s" % driver_name
-            msg += " version is %s" % output.strip().split()[-1]
+            msg += " version is %s" % output.strip()
             test.log.info(msg)
     session.close()

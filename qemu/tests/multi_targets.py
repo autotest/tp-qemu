a6bfe2ab563e47503deaef497e78ed0aaa44b8fd
Houqi (Nick) Zuo <hzuo@redhat.com>
2025-04-16 21:11:49 +0800

    [Draft] Test only
    Signed-off-by: Houqi (Nick) Zuo <hzuo@redhat.com>

---
+++ qemu/tests/multi_targets.py
@@ -0,0 +1,28 @@
+from virttest import data_dir
+from virttest.iscsi import Iscsi
+
+
+def run(test, params, env):
+    """
+    Usage of multi-targets iscsi.
+
+
+    :param test:   QEMU test object.
+    :param params: Dictionary with the test parameters.
+    :param env:    Dictionary with test environment.
+    """
+    base_dir = data_dir.get_data_dir()
+    iscsi = Iscsi.create_iSCSI(params, base_dir)
+    iscsi.login()  # or iscsi.login(target=xxx)
+    # iscsi.export_target()
+    # or
+    # iscsi.export_target(target=xxx)
+    targets = iscsi.query_targets()  # or iscsi.query_targets(target=xxx)
+    test.log.debug("iscsi.query_targets() returns: ")
+    test.log.debug(str(targets))
+
+    dev_name = iscsi.get_device_names()  # or iscsi.get_device_names(target=xxx)
+    test.log.debug("iscsi.get_device_names() returns: ")
+    test.log.debug(str(dev_name))
+
+    iscsi.cleanup()

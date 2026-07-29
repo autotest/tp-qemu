69c7b9de841d450abd01121d123c843acc07102c
Gerd Hoffmann <kraxel@redhat.com>
2026-07-29 09:00:28 +0200

    attestation: add support for snpguest 0.8.x
    Different snpguest versions expect different command lines.  Adjust the
    command lines used depending on the snpguest version.  The 0.8.x
    versions are taken from regular_attestation_workflow_legacy.sh
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- qemu/deps/sev-snp/regular_attestation_workflow.sh
+++ qemu/deps/sev-snp/regular_attestation_workflow.sh
@@ -35,7 +35,8 @@ fetch_retry() {
     return 1
 }
 
-version=$(snpguest -V)
+output="$(snpguest -V)"
+version="${output#snpguest }"
 echo "snpguest version: $version"
 
 # Verify regular attestation workflow on snp guest
@@ -47,15 +48,32 @@ fi
 (set -x; snpguest display report attestation-report.bin)
 check_status "Failed display attestation-report."
 
-# Fetch cert
-fetch_retry "snpguest fetch ca -e vcek pem ./ ${cpu_model}"
-check_status "Failed to fetch CA certificate."
-
-fetch_retry "snpguest fetch vcek -p ${cpu_model} pem ./ attestation-report.bin"
-check_status "Failed to fetch VCEK certificate."
+case "${version#snpguest }" in
+    0.8.*)
+        fetch_retry "snpguest fetch ca -e vcek pem ${cpu_model} ./"
+        check_status "Failed to fetch CA certificate."
+        fetch_retry "snpguest fetch vcek pem ${cpu_model} ./ attestation-report.bin"
+        check_status "Failed to fetch VCEK certificate."
+        ;;
+    *)
+        fetch_retry "snpguest fetch ca -e vcek pem ./ ${cpu_model}"
+        check_status "Failed to fetch CA certificate."
+        fetch_retry "snpguest fetch vcek -p ${cpu_model} pem ./ attestation-report.bin"
+        check_status "Failed to fetch VCEK certificate."
+        ;;
+esac
 
 # Verify certs
 (set -x; snpguest verify certs ./)
 check_status "Failed to verify certificates."
-(set -x; snpguest verify attestation -p ${cpu_model} ./ attestation-report.bin)
-check_status "Failed to verify attestation."
+
+case "$version" in
+    0.8.*)
+        (set -x; snpguest verify attestation ./ attestation-report.bin)
+        check_status "Failed to verify attestation."
+        ;;
+    *)
+        (set -x; snpguest verify attestation -p ${cpu_model} ./ attestation-report.bin)
+        check_status "Failed to verify attestation."
+        ;;
+esac
